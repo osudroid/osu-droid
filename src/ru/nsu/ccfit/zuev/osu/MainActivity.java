2f1cc89b3dcb8b7f68c4bfb9c9c15ac7298efb1c
Widecss <Widecss@users.noreply.github.com>
2020-04-13 17:28:26 +0800

    Refactoring code

--- src/ru/nsu/ccfit/zuev/osu/MainActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/MainActivity.java
@@ -717,12 +717,12 @@ public class MainActivity extends BaseGameActivity implements
                     GlobalManager.getInstance().getEngine().getScene() == GlobalManager.getInstance().getSongMenu().getScene()) {
 
                 //SongMenu 界面按返回按钮（系统按钮）
-                GlobalManager.getInstance().getSongService().setGaming(false);
-                GlobalManager.getInstance().getSongMenu().unbindDataBaseChangedListener();
-                GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
-                if (GlobalManager.getInstance().getSelectedTrack() != null) {
-                    GlobalManager.getInstance().getMainScene().setBeatmap(GlobalManager.getInstance().getSelectedTrack().getBeatmap());
-                }
+                GlobalManager.getInstance().getSongMenu().back();
+//                GlobalManager.getInstance().getSongService().setGaming(false);
+//                GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
+//                if (GlobalManager.getInstance().getSelectedTrack() != null) {
+//                    GlobalManager.getInstance().getMainScene().setBeatmap(GlobalManager.getInstance().getSelectedTrack().getBeatmap());
+//                }
             } else {
 
                 if (GlobalManager.getInstance().getEngine().getScene() instanceof LoadingScreen.LoadingScene) {
--- src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
+++ src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
@@ -365,10 +365,11 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
                         }
                         if (moved == false) {
                             backButton.setScale(1f);
-                            unbindDataBaseChangedListener();
-                            GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
-                            GlobalManager.getInstance().getSongService().setGaming(false);
-                            GlobalManager.getInstance().getMainScene().setBeatmap(selectedTrack.getBeatmap());
+                            back();
+//                            unbindDataBaseChangedListener();
+//                            GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
+//                            GlobalManager.getInstance().getSongService().setGaming(false);
+//                            GlobalManager.getInstance().getMainScene().setBeatmap(selectedTrack.getBeatmap());
                         }
                         return true;
                     }
@@ -415,10 +416,10 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
                         }
                         if (moved == false) {
                             backButton.setScale(1f);
-                            unbindDataBaseChangedListener();
-                            GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
-                            GlobalManager.getInstance().getSongService().setGaming(false);
-                            GlobalManager.getInstance().getMainScene().setBeatmap(selectedTrack.getBeatmap());
+                            back();
+//                            GlobalManager.getInstance().getEngine().setScene(GlobalManager.getInstance().getMainScene().getScene());
+//                            GlobalManager.getInstance().getSongService().setGaming(false);
+//                            GlobalManager.getInstance().getMainScene().setBeatmap(selectedTrack.getBeatmap());
                         }
                         return true;
                     }
@@ -739,24 +740,26 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
 
     public void onUpdate(final float pSecondsElapsed) {
         secPassed += pSecondsElapsed;
-        if (GlobalManager.getInstance().getSongService() != null) {
-            synchronized (musicMutex) {
-                if (GlobalManager.getInstance().getSongService() != null && GlobalManager.getInstance().getSongService().getStatus() == Status.PLAYING && GlobalManager.getInstance().getSongService().getVolume() < 1) {
-                    float vol = Math.min(1, GlobalManager.getInstance().getSongService().getVolume() + 0.01f);
-                    GlobalManager.getInstance().getSongService().setVolume(vol);
-                }
-            }
-        }
-
-        if (bg != null) {
-            synchronized (backgroundMutex) {
-                if (bg != null && bg.getRed() < 1) {
-                    final float col = Math
-                            .min(1, bg.getRed() + pSecondsElapsed);
-                    bg.setColor(col, col, col);
-                }
-            }
-        }
+//        if (GlobalManager.getInstance().getSongService() != null) {
+//            synchronized (musicMutex) {
+//                if (GlobalManager.getInstance().getSongService() != null && GlobalManager.getInstance().getSongService().getStatus() == Status.PLAYING && GlobalManager.getInstance().getSongService().getVolume() < 1) {
+//                    float vol = Math.min(1, GlobalManager.getInstance().getSongService().getVolume() + 0.01f);
+//                    GlobalManager.getInstance().getSongService().setVolume(vol);
+//                }
+//            }
+//        }
+//
+//        if (bg != null) {
+//            synchronized (backgroundMutex) {
+//                if (bg != null && bg.getRed() < 1) {
+//                    final float col = Math
+//                            .min(1, bg.getRed() + pSecondsElapsed);
+//                    bg.setColor(col, col, col);
+//                }
+//            }
+//        }
+        increaseVolume();
+        increaseBackgroundLuminance(pSecondsElapsed);
 
         secondsSinceLastSelect += pSecondsElapsed;
         float oy = -camY;
@@ -784,6 +787,49 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
             velocityY = 0;
         }
 
+//        if (selectedItem != null) {
+//            if (selectedItem.percentAppeared < 1) {
+//                selectedItem.percentAppeared += 2 * pSecondsElapsed;
+//            } else {
+//                selectedItem.percentAppeared = 1;
+//            }
+//            selectedItem.update(pSecondsElapsed);
+//        }
+        expandSelectedItem(pSecondsElapsed);
+
+        board.update(pSecondsElapsed);
+//        scrollbar.setPosition(camY + Config.getRES_HEIGHT() / 2, oy);
+//        if (Math.abs(velocityY) > Utils.toRes(500)) {
+//            scrollbar.setVisible(true);
+//        } else {
+//            scrollbar.setVisible(false);
+//        }
+        updateScrollbar(camY + Config.getRES_HEIGHT() / 2, oy);
+    }
+
+    public void increaseVolume(){
+        if (GlobalManager.getInstance().getSongService() != null) {
+            synchronized (musicMutex) {
+                if (GlobalManager.getInstance().getSongService() != null && GlobalManager.getInstance().getSongService().getStatus() == Status.PLAYING && GlobalManager.getInstance().getSongService().getVolume() < 1) {
+                    float vol = Math.min(1, GlobalManager.getInstance().getSongService().getVolume() + 0.01f);
+                    GlobalManager.getInstance().getSongService().setVolume(vol);
+                }
+            }
+        }
+    }
+
+    public void increaseBackgroundLuminance(final float pSecondsElapsed){
+        if (bg != null) {
+            synchronized (backgroundMutex) {
+                if (bg != null && bg.getRed() < 1) {
+                    final float col = Math.min(1, bg.getRed() + pSecondsElapsed);
+                    bg.setColor(col, col, col);
+                }
+            }
+        }
+    }
+
+    public void expandSelectedItem(float pSecondsElapsed){
         if (selectedItem != null) {
             if (selectedItem.percentAppeared < 1) {
                 selectedItem.percentAppeared += 2 * pSecondsElapsed;
@@ -792,9 +838,10 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
             }
             selectedItem.update(pSecondsElapsed);
         }
+    }
 
-        board.update(pSecondsElapsed);
-        scrollbar.setPosition(camY + Config.getRES_HEIGHT() / 2, oy);
+    public void updateScrollbar(final float vy, final float maxy) {
+        scrollbar.setPosition(vy, maxy);
         if (Math.abs(velocityY) > Utils.toRes(500)) {
             scrollbar.setVisible(true);
         } else {
@@ -1099,6 +1146,11 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
     }
 
     public void unload() {
+    }
+
+    public void back() {
+        unbindDataBaseChangedListener();
+        GlobalManager.getInstance().getMainScene().show();
     }
 
     public void bindDataBaseChangedListener(){
