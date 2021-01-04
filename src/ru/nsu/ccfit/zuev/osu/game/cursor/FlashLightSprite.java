b5bbd556b27a13fd3616e5a9fd1a3018a9fe979f
el-dasher <el-dasher@users.noreply.github.com>
2021-01-03 22:06:44 -0400

    Better FL... (read desc)
    Buffed FL so it's area is more similar to the osu!std one, still quite easier but idk,  also made FL slider shrinking area be a fixed number since in osu!std it's like that too...

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightSprite.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightSprite.java
@@ -4,19 +4,21 @@ import org.anddev.andengine.entity.Entity;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import ru.nsu.ccfit.zuev.osu.ResourceManager;
 
 public class FlashLightSprite extends Entity{
-    private Sprite sprite;
+    private final Sprite sprite;
     private boolean showing = false;
-    private float size = 8f;
+    private final float size = 8f;
     private float holdX = 0f;
     private float holdY = 0f;
-    private float leadScale = 0f;
 
     public FlashLightSprite() {
         TextureRegion tex = ResourceManager.getInstance().getTexture("flashlight_cursor");
-        sprite = new Sprite(-tex.getWidth() / 2, -tex.getHeight() / 2, tex);
+        sprite = new Sprite(-tex.getWidth() / 2.0f, -tex.getHeight() / 2.0f, tex);
         sprite.setScale(size);
         attachChild(sprite);
     }
@@ -42,24 +44,16 @@ public class FlashLightSprite extends Entity{
             else
                 sprite.setVisible(false);
         }
-        if (combo > 200) {
-            sprite.setScale(0.6f * size);
-            leadScale = 0.6f;
-        } else if (combo > 100) {
-            sprite.setScale(0.8f * size);
-            leadScale = 0.8f;
+        float leadScale;
+        if (combo >= 200) {
+            leadScale = 0.5f;
+        } else if (combo >= 100) {
+            leadScale = 0.7f;
         } else {
-            sprite.setScale(1f * size);
-            leadScale = 1.0f;
+            leadScale = 0.9f;
         }
-        if (holdX != 0 && holdY != 0) {
-            if (leadScale == 1.0f) {
-                sprite.setScale(0.8f * size);
-            } else if (leadScale == 0.8f) {
-                sprite.setScale(0.6f * size);
-            } else {
-                sprite.setScale(0.4f * size);
-            }
-        }
+        if (holdX == 0 && holdY == 0) {
+            sprite.setScale(0.4f * size);
+        } else { sprite.setScale(leadScale * size); }
     }
 }
