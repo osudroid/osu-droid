43449b8a54dd7fc583b8be00daf4b62c0408679a
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-04-09 01:28:05 -0400

    removed debug "junk"

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightDimLayer.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightDimLayer.java
@@ -1,16 +1,9 @@
 package ru.nsu.ccfit.zuev.osu.game.cursor;
 
 
-import android.view.animation.AlphaAnimation;
-
 import org.anddev.andengine.entity.Entity;
-import org.anddev.andengine.entity.modifier.AlphaModifier;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
-import org.anddev.andengine.util.Debug;
-
-import javax.microedition.khronos.opengles.GL10;
-
 import ru.nsu.ccfit.zuev.osu.ResourceManager;
 
 public class FlashLightDimLayer extends Entity {
