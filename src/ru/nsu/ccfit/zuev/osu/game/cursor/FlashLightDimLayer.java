8292cbf073b7aacfabccb213447c2550fa39e879
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-04-09 14:09:29 -0400

    Small optimization

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightDimLayer.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightDimLayer.java
@@ -4,6 +4,7 @@ package ru.nsu.ccfit.zuev.osu.game.cursor;
 import org.anddev.andengine.entity.Entity;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
 import ru.nsu.ccfit.zuev.osu.ResourceManager;
 
 public class FlashLightDimLayer extends Entity {
@@ -14,10 +15,16 @@ public class FlashLightDimLayer extends Entity {
         sprite = new Sprite(-tex.getWidth() / 2f, -tex.getHeight() / 2f, tex);
         float size = 8f;
         sprite.setScale(size);
+        sprite.setVisible(false);
         attachChild(sprite);
     }
 
     public void update(boolean isSliderHold) {
-        sprite.setVisible(isSliderHold);
+        boolean isSpriteVisible = sprite.isVisible();
+        if (isSliderHold && !isSpriteVisible) {
+            sprite.setVisible(true);
+        } else if (!isSliderHold && isSpriteVisible) {
+            sprite.setVisible(false);
+        }
     }
 }
