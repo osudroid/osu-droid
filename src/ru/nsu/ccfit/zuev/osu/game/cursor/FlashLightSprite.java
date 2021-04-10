0a84e66f9abacf43d2e9a624dc087ce72dc77305
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-04-10 00:01:03 -0400

    Update FlashLightSprite.java

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightSprite.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/FlashLightSprite.java
@@ -1,6 +1,7 @@
 package ru.nsu.ccfit.zuev.osu.game.cursor;
 
 import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.modifier.ScaleModifier;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
@@ -11,14 +12,15 @@ public class FlashLightSprite extends Entity{
     private final Sprite sprite;
     private final FlashLightDimLayer dimLayer;
     private boolean showing = false;
-    private final float size = 8f;
     private boolean isSliderHold = false;
+    private final float baseSize = 8f;
+    private float currentSize = baseSize;
 
     public FlashLightSprite(Scene fgScene) {
         TextureRegion tex = ResourceManager.getInstance().getTexture("flashlight_cursor");
         sprite = new Sprite(-tex.getWidth() / 2f, -tex.getHeight() / 2f, tex);
         dimLayer = new FlashLightDimLayer();
-        sprite.setScale(size);
+        sprite.setScale(baseSize);
         attachChild(sprite);
         fgScene.attachChild(dimLayer, 0);
     }
@@ -34,22 +36,27 @@ public class FlashLightSprite extends Entity{
     public void update(float pSecondsElapsed, int combo) {
         if (showing){
             dimLayer.update(this.isSliderHold);
-            if (!sprite.isVisible())
+            if (!sprite.isVisible()) {
                 sprite.setVisible(true);
-            if (sprite.getAlpha() < 1)
+            }
+            if (sprite.getAlpha() < 1) {
                 sprite.setAlpha(1);
+            }
         } else {
-            if (sprite.getAlpha() > 0)
+            if (sprite.getAlpha() > 0) {
                 sprite.setAlpha(Math.max(0, sprite.getAlpha() - 4f * pSecondsElapsed));
-            else
+            } else {
                 sprite.setVisible(false);
+            }
         }
-        if (combo > 200) {
-            sprite.setScale(0.6f * size);
-        } else if (combo > 100){
-           sprite.setScale(0.8f * size);
-        } else {
-            sprite.setScale(1f * size);
+
+        // Area stops shrinking at 200 combo
+        if (combo <= 200 && combo % 100 == 0) {
+            // For every 100 combo, the size is decreased by 20%
+            final float newSize = (1 - 0.2f * combo / 100f) * baseSize;
+            ScaleModifier sm = new ScaleModifier(0.5f, currentSize, newSize);
+            sprite.registerEntityModifier(sm);
+            currentSize = newSize;
         }
 
     }
