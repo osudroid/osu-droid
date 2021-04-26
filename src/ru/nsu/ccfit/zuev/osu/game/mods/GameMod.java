01b22135ff7ca51ff260b8fd7e47b3c392a621e3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-04-26 23:07:22 +0700

    Add isRanked property

--- src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
+++ src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
@@ -1,38 +1,42 @@
 package ru.nsu.ccfit.zuev.osu.game.mods;
 
 public enum GameMod {
-    MOD_NOFAIL("nf", 0.5f),
-    MOD_AUTO("auto", 0, true),
-    MOD_EASY("es", 0.5f),
-    MOD_HARDROCK("hr", 1.06f),
-    MOD_HIDDEN("hd", 1.06f),
-    MOD_RELAX("relax", 0.001f, true),
-    MOD_AUTOPILOT("ap", 0.001f, true),
-    MOD_DOUBLETIME("dt", 1.12f),
-    MOD_NIGHTCORE("nc", 1.12f),
-    MOD_HALFTIME("ht", 0.3f),
-    MOD_SUDDENDEATH("sd", 1),
-    MOD_PERFECT("pf", 1),
-    MOD_FLASHLIGHT("fl", 1.12f),
-    MOD_PRECISE("pr", 1.06f),
-    MOD_SMALLCIRCLE("sc", 1.06f),
-    MOD_REALLYEASY("re", 0.5f),
-    MOD_SCOREV2("v2", 1),
-    MOD_SPEEDUP("su", 1.06f);
+    MOD_NOFAIL("nf", 0.5f, true),
+    MOD_AUTO("auto", 0, false, true),
+    MOD_EASY("es", 0.5f, true),
+    MOD_HARDROCK("hr", 1.06f, true),
+    MOD_HIDDEN("hd", 1.06f, true),
+    MOD_RELAX("relax", 0.001f, false, true),
+    MOD_AUTOPILOT("ap", 0.001f, false, true),
+    MOD_DOUBLETIME("dt", 1.12f, true),
+    MOD_NIGHTCORE("nc", 1.12f, true),
+    MOD_HALFTIME("ht", 0.3f, true),
+    MOD_SUDDENDEATH("sd", 1, false),
+    MOD_PERFECT("pf", 1, false),
+    MOD_FLASHLIGHT("fl", 1.12f, false),
+    MOD_PRECISE("pr", 1.06f, true),
+    MOD_SMALLCIRCLE("sc", 1.06f, false),
+    MOD_REALLYEASY("re", 0.5f, false),
+    MOD_SCOREV2("v2", 1, false),
+    MOD_SPEEDUP("su", 1.06f, false);
     
     public final String shortName;
     public final float scoreMultiplier;
+    public final boolean isRanked;
     public final boolean typeAuto;
 
-    GameMod(String shortName, float scoreMultiplier) {
+    GameMod(String shortName, float scoreMultiplier, boolean isRanked) {
         this.shortName = shortName;
         this.scoreMultiplier = scoreMultiplier;
+        this.isRanked = isRanked;
         this.typeAuto = false;
     }
 
-    GameMod(String shortName, float scoreMultiplier, boolean typeAuto) {
+    GameMod(String shortName, float scoreMultiplier, boolean isRanked, boolean typeAuto) {
         this.shortName = shortName;
         this.scoreMultiplier = scoreMultiplier;
+        // Generally, autoplaying mods should not be ranked
+        this.isRanked = !typeAuto && isRanked;
         this.typeAuto = typeAuto;
     }
 }
