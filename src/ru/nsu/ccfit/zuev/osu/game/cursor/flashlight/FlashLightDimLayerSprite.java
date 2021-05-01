cbb9c871b796c523fb9622f093b16b11e38a85f0
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-05-01 13:14:04 -0400

    Apply some changes
    (rian) I am sorry for not checking this b4
    
    Please refer to this:
    https://github.com/ppy/osu/pull/4617/commits/8f101e4f604bac362799fe8c171b8cdaee4aef28
    
    And this: https://github.com/ppy/osu/blob/master/osu.Game.Rulesets.Osu/Mods/OsuModFlashlight.cs#L69

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/flashlight/FlashLightDimLayerSprite.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/flashlight/FlashLightDimLayerSprite.java
@@ -4,22 +4,18 @@ import android.graphics.Color;
 import com.edlplan.andengine.TextureHelper;
 
 import org.anddev.andengine.entity.modifier.AlphaModifier;
-import org.anddev.andengine.entity.modifier.FadeOutModifier;
 
 public class FlashLightDimLayerSprite extends FlashlightAreaSizedSprite {
-    public final float SLIDER_DIM_ALPHA = 0.75f;
+    public final float BASE_SLIDER_DIM_ALPHA = 0.8f;
 
     public FlashLightDimLayerSprite() {
         super(TextureHelper.create1xRegion(Color.BLACK));
         this.setScale(MainFlashLightSprite.TEXTURE_WIDTH, MainFlashLightSprite.TEXTURE_HEIGHT);
-        this.setAlpha(SLIDER_DIM_ALPHA);
+        this.setAlpha(BASE_SLIDER_DIM_ALPHA);
     }
 
     public void update(boolean isSliderDimActive) {
-        if (isSliderDimActive) {
-            this.setAlpha(SLIDER_DIM_ALPHA);
-        } else if (this.getAlpha() == SLIDER_DIM_ALPHA) {
-            this.registerEntityModifier(new FadeOutModifier(0.05f));
-        }
+       float newAlpha = isSliderDimActive? BASE_SLIDER_DIM_ALPHA : 0;
+       this.registerEntityModifier(new AlphaModifier(0.05f, this.getAlpha(), newAlpha));
     }
 }
