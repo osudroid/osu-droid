ff0c267429749e23f2b085abe34701fae74d92f3
kairusds <harveyhansjavier@gmail.com>
2021-10-16 13:45:40 +0800

    Update SongService notification to be compatible with recent versions of Android

--- src/ru/nsu/ccfit/zuev/audio/serviceAudio/SaveServiceObject.java
+++ src/ru/nsu/ccfit/zuev/audio/serviceAudio/SaveServiceObject.java
@@ -1,18 +1,13 @@
 package ru.nsu.ccfit.zuev.audio.serviceAudio;
 
-
 import android.app.Activity;
 import android.app.Application;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.umeng.analytics.MobclickAgent;
-import com.umeng.commonsdk.UMConfigure;
-
 import ru.nsu.ccfit.zuev.osu.AppException;
 import ru.nsu.ccfit.zuev.osu.GlobalManager;
-import ru.nsu.ccfit.zuev.osuplus.BuildConfig;
 
 public class SaveServiceObject extends Application {
 
@@ -48,56 +43,40 @@ public class SaveServiceObject extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //注册App异常崩溃处理器
-        UMConfigure.init(this,
-                BuildConfig.DEBUG ?
-                        "5fccbf9d19bda368eb483d62": // Debug key
-                        "5fe567c044bb94418a649888", // Release key
-                String.format("%s[%s]", BuildConfig.FLAVOR, BuildConfig.BUILD_TYPE),
-                UMConfigure.DEVICE_TYPE_PHONE, "");
-        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
         Thread.setDefaultUncaughtExceptionHandler(AppException.getAppExceptionHandler());
-        if (Build.VERSION.SDK_INT > 14) {
-            registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-                @Override
-                public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            }
 
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                Log.w("onActivityDestroyed", "I'm going to Dead O_x");
+                if (songService != null) {
+                    Log.w("onActivityDestroyed", "I'm Dead x_x");
+                    songService.hideNotifyPanel();
                 }
-
-                @Override
-                public void onActivityStarted(Activity activity) {
-
-                }
-
-                @Override
-                public void onActivityResumed(Activity activity) {
-
-                }
-
-                @Override
-                public void onActivityPaused(Activity activity) {
-
-                }
-
-                @Override
-                public void onActivityStopped(Activity activity) {
-
-                }
-
-                @Override
-                public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-                }
-
-                @Override
-                public void onActivityDestroyed(Activity activity) {
-                    Log.w("onActivityDestroyed", "I'm going to Dead O_x");
-                    if (songService != null) {
-                        Log.w("onActivityDestroyed", "I'm Dead x_x");
-                        songService.hideNotifyPanel();
-                    }
-                }
-            });
-        }
+            }
+        });
     }
 }
