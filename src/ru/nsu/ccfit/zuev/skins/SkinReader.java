3c0ce22d680881636dc83657a584e5a4b44f7517
o-dasher <88356162+o-dasher@users.noreply.github.com>
2021-10-16 18:42:28 -0300

    just small refactor ig

--- src/ru/nsu/ccfit/zuev/skins/SkinReader.java
+++ src/ru/nsu/ccfit/zuev/skins/SkinReader.java
@@ -16,5 +16,7 @@ public abstract class SkinReader {
     protected abstract void loadLayout();
     protected abstract void loadColor();
     protected abstract void loadCursor();
-    protected abstract void putLayout(String name, JSONObject object);
+    protected void putLayout(String name, SkinLayout layout) {
+        OsuSkin.get().layoutData.put(name, layout);
+    }
 }
