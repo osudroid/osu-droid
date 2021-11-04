ad887dc91cc41021b6d2a85c88453e0274332722
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2021-11-04 16:36:46 +0700

    Fix illegal expression start error

--- src/ru/nsu/ccfit/zuev/osu/MainActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/MainActivity.java
@@ -528,6 +528,8 @@ public class MainActivity extends BaseGameActivity implements
                 }
             }
         }
+    }
+
     public Handler getHandler() {
         return handler;
     }
