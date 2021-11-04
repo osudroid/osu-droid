cdd182731686ace331139a349a39cff6692f7579
harveyhans <harveyhansjavier@gmail.com>
2021-11-04 18:06:37 +0800

    Use zip4j for SkinImporter
    And use LibraryManager.deleteDir for deleting the folder when the osk file is malformed.

--- src/ru/nsu/ccfit/zuev/osu/SkinImporter.java
+++ src/ru/nsu/ccfit/zuev/osu/SkinImporter.java
@@ -3,12 +3,10 @@ package ru.nsu.ccfit.zuev.osu;
 import org.anddev.andengine.util.Debug;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import ru.nsu.ccfit.zuev.osu.helper.StringTable;
 import ru.nsu.ccfit.zuev.osuplus.R;
@@ -40,50 +38,16 @@ public class SkinImporter {
 
         final String folderName = osk.getName().substring(0, osk.getName().length() - 4);
 
-        final ArrayList<File> list = new ArrayList<>();
-
         final File folderFile = new File(Config.getSkinTopPath() + folderName);
-
-        if (folderFile.mkdirs()) {
-            list.add(folderFile);
+        if(!folderFile.exists()) {
+            folderFile.mkdirs();
         }
 
         try {
-            // Read file as a zip archive
-            final ZipInputStream inputStream = new ZipInputStream(new FileInputStream(osk));
-
-            for (ZipEntry entry = inputStream.getNextEntry(); entry != null; entry = inputStream
-                    .getNextEntry()) {
-                // Opening a file according to entry information
-                final File entryFile = new File(folderFile, entry.getName());
-
-                if (entry.isDirectory()) {
-                    if (entryFile.mkdirs()) {
-                        list.add(entryFile);
-                    }
-                } else if (entryFile.createNewFile()) {
-                    list.add(entryFile);
-
-                    final FileOutputStream entryStream = new FileOutputStream(entryFile);
-
-                    // Writing data from zip stream to output stream
-                    final byte[] buff = new byte[4096];
-                    int len;
-                    while ((len = inputStream.read(buff)) > 0) {
-                        entryStream.write(buff, 0, len);
-                    }
-
-                    // Closing output stream
-                    entryStream.close();
-                }
-            }
-
-            // Closing .osk file stream
-            inputStream.close();
-
+            new ZipFile(osk).extractAll(folderFile.getAbsolutePath());
             // Delete .osk file
             osk.delete();
-        } catch (final FileNotFoundException e) {
+        } catch (final ZipException e) {
             Debug.e("SkinImporter.importSkin: " + e.getMessage(), e);
             return false;
         } catch (final Exception e) {
@@ -92,9 +56,7 @@ public class SkinImporter {
                     false);
             Debug.e("SkinImporter.importSkin: ", e);
             osk.renameTo(new File(osk.getParentFile(), osk.getName() + ".badosk"));
-            for (int i = list.size() - 1; i >= 0; i--) {
-                list.get(i).delete();
-            }
+            LibraryManager.getInstance().deleteDir(folderFile);
             return false;
         }
 
