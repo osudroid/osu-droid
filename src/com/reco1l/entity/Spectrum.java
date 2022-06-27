5a513d71b1aaf317d27f0c873844599b06411d7e
reco1l <geronimoferruccio15@gmail.com>
2022-06-26 22:53:09 -0300

    Added animation to Spectrum clearing

--- src/com/reco1l/entity/Spectrum.java
+++ src/com/reco1l/entity/Spectrum.java
@@ -8,6 +8,7 @@ import android.os.Build;
 
 import androidx.palette.graphics.Palette;
 
+import org.anddev.andengine.entity.modifier.AlphaModifier;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
 
@@ -36,14 +37,16 @@ public class Spectrum {
     public void draw(Scene scene) {
         for (int i = 0; i < lines; i++) {
             spectrum[i] = new Rectangle((lineWidth + lineDistance) * i, 0, lineWidth, 0);
-            scene.attachChild(spectrum[i]);
+            spectrum[i].setAlpha(0);
+            scene.attachChild(spectrum[i], 1);
         }
     }
 
     public void clear() {
-        for (Rectangle line: spectrum) {
-            line.setHeight(0);
-            line.setAlpha(0);
+        for (int i = 0; i < lines; i++) {
+            spectrum[i].clearEntityModifiers();
+            final float currentAlpha = spectrum[i].getAlpha();
+            spectrum[i].registerEntityModifier(new AlphaModifier(0.5f, currentAlpha, 0));
         }
     }
 
@@ -59,10 +62,10 @@ public class Spectrum {
             if (peak < fft[1 + i])
                 peak = fft[1 + i];
 
-            float CurrentPeakLevel = peak * peakMultiplier;
+            float currentPeakLevel = peak * peakMultiplier;
 
-            if (CurrentPeakLevel > peakLevel[i]) {
-                peakLevel[i] = CurrentPeakLevel;
+            if (currentPeakLevel > peakLevel[i]) {
+                peakLevel[i] = currentPeakLevel;
                 peakDownRate[i] = peakLevel[i] / 20;
             } else {
                 peakLevel[i] = Math.max(peakLevel[i] - peakDownRate[i], 0f);
@@ -70,13 +73,17 @@ public class Spectrum {
             spectrum[i].setHeight(peakLevel[i]);
             spectrum[i].setPosition((lineWidth + lineDistance) * i,
                     Config.getRES_HEIGHT() - spectrum[i].getHeight());
-            spectrum[i].setAlpha(alpha);
+
+            if (spectrum[i].getAlpha() != alpha) {
+                spectrum[i].clearEntityModifiers();
+                spectrum[i].setAlpha(alpha);
+            }
         }
     }
 
     //This method change the spectrum color according to the background luminance, using androidx.palette library.
     public void updateColor(TrackInfo currentTrack) {
-        if (Build.VERSION.SDK_INT < 24) //todo Replace Color.luminance with some algorithm.
+        if (Build.VERSION.SDK_INT < 24)
             return;
 
         Drawable drawable = Drawable.createFromPath(currentTrack.getBackground());
@@ -91,7 +98,7 @@ public class Spectrum {
             color = palette.getDominantColor(0x000000);
         }
 
-        //Color.luminance isn't compatible with API < 24.
+        //Color.luminance is not compatible with API < 24.
         //This can be replaced with some algorithm to get color luminance.
         boolean isDarker = Color.luminance(color) < 0.5f;
 
@@ -103,6 +110,6 @@ public class Spectrum {
         }
 
         if (bitmap != null)
-            bitmap.recycle(); //This recycles the bitmap to avoid memory leaks.
+            bitmap.recycle(); //Recycling the bitmap to avoid memory leaks.
     }
 }
