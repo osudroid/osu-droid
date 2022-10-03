a3de05015a8773c51f0af025710c8189537ec9e6
Likver <anonymatebg@gmail.com>
2022-10-04 00:30:58 +0200

    1

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/main/CursorEntity.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/main/CursorEntity.java
@@ -29,7 +29,7 @@ public class CursorEntity extends Entity {
 
             emitter = new PointParticleEmitter(particleOffsetX, particleOffsetY);
             particles = new CursorTrail(
-                    emitter, 10, 15, 20, cursorSprite.baseSize, trailTex
+                    emitter, 100, 50, 1, cursorSprite.baseSize, trailTex
             );
         }
 
