ef172ccce076d9664b83b15f6112eb630208918a
Reco1l <geronimoferruccio15@gmail.com>
2023-01-21 19:44:19 +0100

    Update Gradle to 7.6

--- src/com/reco1l/interfaces/IReferences.java
+++ src/com/reco1l/interfaces/IReferences.java
@@ -1,6 +1,10 @@
 package com.reco1l.interfaces;
 
 import com.reco1l.GameEngine;
+import com.reco1l.scenes.LoaderScene;
+import com.reco1l.scenes.MainScene;
+import com.reco1l.scenes.SelectorScene;
+import com.reco1l.scenes.SummaryScene;
 import com.reco1l.ui.FragmentPlatform;
 import com.reco1l.management.BeatmapCollection;
 import com.reco1l.management.TimingWrapper;
@@ -12,6 +16,7 @@ import ru.nsu.ccfit.zuev.osu.LibraryManager;
 import ru.nsu.ccfit.zuev.osu.MainActivity;
 import ru.nsu.ccfit.zuev.osu.PropertiesLibrary;
 import ru.nsu.ccfit.zuev.osu.ResourceManager;
+import ru.nsu.ccfit.zuev.osu.game.GameScene;
 import ru.nsu.ccfit.zuev.osu.menu.ModMenu;
 import ru.nsu.ccfit.zuev.osu.online.OnlineManager;
 import ru.nsu.ccfit.zuev.osu.online.OnlineScoring;
@@ -22,10 +27,7 @@ import ru.nsu.ccfit.zuev.skins.SkinManager;
 
 public interface IReferences {
 
-    GameEngine engine = GameEngine.getInstance();
-    MainActivity activity = MainActivity.getInstance();
-    FragmentPlatform platform = FragmentPlatform.getInstance();
-
+    // OLD
     ModMenu modMenu = ModMenu.getInstance();
 
     SkinManager skinManager = SkinManager.getInstance();
@@ -38,10 +40,4 @@ public interface IReferences {
 
     OnlineManager onlineManager = OnlineManager.getInstance();
     OnlineScoring onlineScoring = OnlineScoring.getInstance();
-
-    MusicManager musicManager = MusicManager.getInstance();
-    TimingWrapper timingWrapper = TimingWrapper.getInstance();
-    BitmapManager bitmapManager = BitmapManager.getInstance();
-
-    BeatmapCollection beatmapCollection = BeatmapCollection.getInstance();
 }
