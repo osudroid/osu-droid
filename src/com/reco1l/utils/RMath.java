d7a76088214a26566bdd021511095000fef8cb66
Reco1l <geronimoferruccio15@gmail.com>
2023-02-07 04:05:03 +0100

    Added basic math operation util class

---
+++ src/com/reco1l/utils/RMath.java
@@ -0,0 +1,40 @@
+package com.reco1l.utils;
+
+// Reco1l math xd
+public final class RMath {
+
+    //--------------------------------------------------------------------------------------------//
+
+    private RMath() {}
+
+    //--------------------------------------------------------------------------------------------//
+    // Percentage of a value
+
+    public static int pct(int value, float percent) {
+        return (int) (value * (percent / 100f));
+    }
+
+    public static float pct(float value, float percent) {
+        return value * (percent / 100f);
+    }
+
+    //--------------------------------------------------------------------------------------------//
+
+    public static int half(int value) {
+        return value / 2;
+    }
+
+    public static float half(float value) {
+        return value / 2;
+    }
+
+    //--------------------------------------------------------------------------------------------//
+
+    public static int quart(int value) {
+        return value / 4;
+    }
+
+    public static float quart(float value) {
+        return value / 4;
+    }
+}
