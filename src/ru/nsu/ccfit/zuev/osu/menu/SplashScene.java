68eb80604df1f7f82475dfdbd06f1d514e073728
Reco1l <geronimoferruccio15@gmail.com>
2023-05-10 03:27:23 +0200

    Improve intro animations

--- src/ru/nsu/ccfit/zuev/osu/menu/SplashScene.java
+++ src/ru/nsu/ccfit/zuev/osu/menu/SplashScene.java
@@ -1,132 +1,115 @@
 package ru.nsu.ccfit.zuev.osu.menu;
 
 import org.anddev.andengine.engine.handler.IUpdateHandler;
-import org.anddev.andengine.entity.modifier.FadeInModifier;
-import org.anddev.andengine.entity.modifier.FadeOutModifier;
-import org.anddev.andengine.entity.modifier.LoopEntityModifier;
-import org.anddev.andengine.entity.modifier.ParallelEntityModifier;
-import org.anddev.andengine.entity.modifier.RotationModifier;
-import org.anddev.andengine.entity.modifier.ScaleModifier;
-import org.anddev.andengine.entity.modifier.SequenceEntityModifier;
-import org.anddev.andengine.entity.primitive.Rectangle;
+import org.anddev.andengine.entity.modifier.*;
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.scene.background.ColorBackground;
-import org.anddev.andengine.entity.scene.background.SpriteBackground;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.text.ChangeableText;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
-import org.anddev.andengine.util.HorizontalAlign;
-import org.anddev.andengine.util.modifier.ease.EaseBounceOut;
-import org.anddev.andengine.util.modifier.ease.EaseElasticOut;
 
+import org.anddev.andengine.util.HorizontalAlign;
 import ru.nsu.ccfit.zuev.osu.Config;
 import ru.nsu.ccfit.zuev.osu.GlobalManager;
 import ru.nsu.ccfit.zuev.osu.ResourceManager;
-import ru.nsu.ccfit.zuev.osu.Utils;
 
 /**
  * Created by Fuuko on 2015/4/25.
  */
 public class SplashScene implements IUpdateHandler {
+
+    public static final SplashScene INSTANCE = new SplashScene();
+
     private final Scene scene;
-    private Rectangle progressRect;
-    private Rectangle bgRect;
+
     private ChangeableText infoText;
-    private ChangeableText progressText;
-    private float currentProgressWidth = 0;
+    private Sprite mLoading;
+
+    private boolean mStarting = true;
 
     public SplashScene() {
         scene = new Scene();
-        scene.registerEntityModifier(new FadeOutModifier(0.5f));
-        initializeLogo();
-        initializeBackground();
-        initializeProgress();
+        initializeLoading();
         initializeInfo();
         scene.registerUpdateHandler(this);
     }
-    private void initializeLogo() {
-        final TextureRegion logoTexture = ResourceManager.getInstance().getTexture("logo");
-        float targetScale = 0.7f;
-        float scaleDuration = 1f;
 
-        Sprite logo = new Sprite(0, 0, logoTexture);
-        logo.setWidth(500);
-        logo.setHeight(500);
-        logo.setPosition((Config.getRES_WIDTH() - logoTexture.getWidth()) / 2f, (Config.getRES_HEIGHT() - logoTexture.getHeight()) / 2f - 40);
+    private void initializeLoading() {
+        var loadTex = ResourceManager.getInstance().getTexture("loading_start");
 
-        ScaleModifier scaleUpModifier = new ScaleModifier(scaleDuration, 0.6f, targetScale, EaseElasticOut.getInstance());
-        ScaleModifier scaleDownModifier = new ScaleModifier(scaleDuration, targetScale, 0.6f, EaseElasticOut.getInstance());
-        SequenceEntityModifier pulseModifier = new SequenceEntityModifier(scaleUpModifier, scaleDownModifier);
-        LoopEntityModifier loopModifier = new LoopEntityModifier(pulseModifier);
-        RotationModifier rotateModifier = new RotationModifier(1f, 0f, 360f, EaseBounceOut.getInstance());
-        ParallelEntityModifier parallelModifier = new ParallelEntityModifier(loopModifier, rotateModifier);
-        FadeInModifier fadeInModifier = new FadeInModifier(0.1f);
-        FadeOutModifier fadeOutModifier = new FadeOutModifier(0.1f);
-        SequenceEntityModifier sequenceModifier = new SequenceEntityModifier(fadeInModifier, parallelModifier, fadeOutModifier);
+        mLoading = new Sprite(0, 0, loadTex);
+        mLoading.setPosition((Config.getRES_WIDTH() - mLoading.getWidth()) / 2f, (Config.getRES_HEIGHT() - mLoading.getHeight()) / 2f);
+        mLoading.setRotationCenter(mLoading.getWidth() / 2f, mLoading.getHeight() / 2f);
+        mLoading.setAlpha(0);
 
-        logo.registerEntityModifier(sequenceModifier);
-        scene.attachChild(logo);
+        mLoading.registerEntityModifier(new LoopEntityModifier(new RotationByModifier(2f, 360)));
+        scene.attachChild(mLoading);
     }
 
-    private void initializeBackground() {
-        final TextureRegion bgTexture = ResourceManager.getInstance().getTexture("loading-background");
-
-        if (bgTexture != null) {
-            float height = bgTexture.getHeight() * Config.getRES_WIDTH() / (float) bgTexture.getWidth();
-            final Sprite menuBg = new Sprite(0, (Config.getRES_HEIGHT() - height) / 2, Config.getRES_WIDTH(), height, bgTexture);
-            scene.setBackground(new SpriteBackground(menuBg));
-        } else {
-            scene.setBackground(new ColorBackground(70 / 255f, 129 / 255f, 252 / 255f));
-        }
-    }
-
-    private void initializeProgress() {
-        bgRect = new Rectangle(0, 0, Utils.toRes(800), Utils.toRes(50));
-        bgRect.setPosition((Config.getRES_WIDTH() - bgRect.getWidth()) / 2, Config.getRES_HEIGHT() - 90);
-        bgRect.setColor(0, 0, 0, 0.4f);
-        scene.attachChild(bgRect);
-
-        progressRect = new Rectangle(bgRect.getX(), bgRect.getY(), 0,bgRect.getHeight());
-        scene.attachChild(progressRect);
-
-        progressText = new ChangeableText(0, 0, ResourceManager.getInstance().getFont("font"), "0 %", HorizontalAlign.CENTER, 10);
-        progressText.setPosition((Config.getRES_WIDTH() - progressText.getWidth()) / 2f, bgRect.getY() + bgRect.getHeight() / 2f - progressText.getHeight() / 2f);
-        scene.attachChild(progressText);
-    }
     private void initializeInfo() {
-        infoText = new ChangeableText(0, 0, ResourceManager.getInstance().getFont("strokeFont"), "", HorizontalAlign.CENTER, 1024);
-        infoText.setPosition((Config.getRES_WIDTH() - infoText.getWidth()) / 2, bgRect.getY() - infoText.getHeight() - 10);
+        infoText = new ChangeableText(0, 0, ResourceManager.getInstance().getFont("font"), "", HorizontalAlign.CENTER, 1024);
+        infoText.setPosition((Config.getRES_WIDTH() - infoText.getWidth()) / 2, Config.getRES_HEIGHT() - infoText.getHeight() - 20);
+        infoText.setAlpha(0);
         scene.attachChild(infoText);
     }
 
     public Scene getScene() { return scene; }
 
+    public void playWelcomeAnimation()
+    {
+        mStarting = false;
+
+        // ChangeableText isn't compatible with animations unfortunately
+        infoText.detachSelf();
+        mLoading.registerEntityModifier(new FadeOutModifier(0.2f));
+
+        try
+        {
+            Thread.sleep(220);
+        }
+        catch (InterruptedException ignored)
+        {
+        }
+
+        var welcomeTex = ResourceManager.getInstance().getTexture("welcome");
+        var welcomeSprite = new Sprite(0, 0, ResourceManager.getInstance().getTexture("welcome"));
+
+        var welcomeSound = ResourceManager.getInstance().getSound("welcome");
+        var welcomePiano = ResourceManager.getInstance().getSound("welcome_piano");
+
+        welcomeSprite.setPosition((Config.getRES_WIDTH() - welcomeTex.getWidth()) / 2f, (Config.getRES_HEIGHT() - welcomeTex.getHeight()) / 2f);
+        welcomeSprite.setAlpha(0);
+        welcomeSprite.setScaleY(0);
+        scene.attachChild(welcomeSprite);
+        welcomeSound.play();
+        welcomePiano.play();
+
+        welcomeSprite.registerEntityModifier(new ParallelEntityModifier(
+                new FadeInModifier(2.5f),
+                new SequenceEntityModifier(
+                        new ScaleModifier(0.25f, 1f, 1f, 0f, 1f),
+                        new ScaleModifier(2.25f, 1f, 1.1f)
+                )
+        ));
+    }
+
     @Override
-    public void onUpdate(final float pSecondsElapsed) {
-        float progress = GlobalManager.getInstance().getLoadingProgress();
-        float targetProgressWidth = bgRect.getWidth() * (progress / 100f);
+    public void onUpdate(float pSecondsElapsed) {
 
-        currentProgressWidth += (targetProgressWidth - currentProgressWidth) * 0.1f;
-        progressRect.setWidth(currentProgressWidth);
-
-        progressText.setText(String.format("%.0f %%", progress));
-        progressText.setPosition((Config.getRES_WIDTH() - progressText.getWidth()) / 2, bgRect.getY() + (bgRect.getHeight() - progressText.getHeight()) / 2);
-
-        if (progress < 35) {
-            progressRect.setColor(0.8f, 0, 0);
-        } else if (progress < 85) {
-            progressRect.setColor(0.8f, 0.8f, 0);
-        } else {
-            progressRect.setColor(0, 0.8f, 0);
+        if (mStarting)
+        {
+            mLoading.setAlpha(mLoading.getAlpha() + 0.1f);
         }
 
         if (GlobalManager.getInstance().getInfo() != null) {
             infoText.setText(GlobalManager.getInstance().getInfo());
-            infoText.setPosition((Config.getRES_WIDTH() - infoText.getWidth()) / 2, bgRect.getY() - infoText.getHeight() - 10);
+            infoText.setPosition((Config.getRES_WIDTH() - infoText.getWidth()) / 2, Config.getRES_HEIGHT() - infoText.getHeight() - 20);
         }
     }
 
     @Override
-    public void reset() { }
+    public void reset()
+    {
+
+    }
 }
 
