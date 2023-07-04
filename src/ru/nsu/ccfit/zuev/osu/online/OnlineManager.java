611a845fbf37a5aeb0d62d550f56cc5e89a86278
Acivev <marko-9970@live.de>
2023-07-04 12:34:00 +0200

    increase online version

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -26,7 +26,7 @@ public class OnlineManager {
     public static final String hostname = "osudroid.moe";
     public static final String endpoint = "https://" + hostname + "/api/";
     public static final String defaultAvatarURL = "https://" + hostname + "/user/avatar/0.png";
-    private static final String onlineVersion = "32";
+    private static final String onlineVersion = "33";
 
     public static final OkHttpClient client = new OkHttpClient();
 
