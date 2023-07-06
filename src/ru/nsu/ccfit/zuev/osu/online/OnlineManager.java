4e7a7be3ab511acb3642bca32068e8e34c496c2a
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-07-06 17:39:12 +0800

    Update OnlineManager.java

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -26,7 +26,7 @@ public class OnlineManager {
     public static final String hostname = "osudroid.moe";
     public static final String endpoint = "https://" + hostname + "/api/";
     public static final String defaultAvatarURL = "https://" + hostname + "/user/avatar/0.png";
-    private static final String onlineVersion = "33";
+    private static final String onlineVersion = "odrx1";
 
     public static final OkHttpClient client = new OkHttpClient();
 
