b8c2457d4c6ccbf689fe38a13a838459eb0c920e
Reco1l <geronimoferruccio15@gmail.com>
2023-10-01 06:01:55 +0200

    Fix background dim being applied twice while break

--- src/ru/nsu/ccfit/zuev/osu/game/GameScene.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameScene.java
@@ -6,7 +6,6 @@ import android.os.SystemClock;
 
 import com.edlplan.ext.EdExtensionHelper;
 import com.edlplan.framework.math.FMath;
-import com.edlplan.framework.support.ProxySprite;
 import com.edlplan.framework.support.osb.StoryboardSprite;
 import com.edlplan.framework.utils.functionality.SmartIterator;
 import com.edlplan.osu.support.timing.TimingPoints;
@@ -176,7 +175,7 @@ public class GameScene implements IUpdateHandler, GameObjectListener,
     private float avgOffset;
     private int offsetRegs;
     private Rectangle kiaiRect = null;
-    private Sprite bgSprite = null;
+    private Rectangle dimRectangle = null;
     private Sprite unranked;
     private ChangeableText replayText;
     private String title, artist, version;
@@ -249,12 +248,14 @@ public class GameScene implements IUpdateHandler, GameObjectListener,
     }
 
     private void setBackground() {
-        bgSprite = null;
+        dimRectangle = null;
 
         if (video != null) {
             video.release();
             video = null;
         }
+
+        Sprite bgSprite = null;
 
         if (Config.isVideoEnabled() && beatmapData.events.videoFilename != null
                 // Unfortunately MediaPlayer API doesn't allow to change playback speed on APIs < 23, so in that case
@@ -310,7 +311,7 @@ public class GameScene implements IUpdateHandler, GameObjectListener,
             storyboardSprite = null;
         }
 
-        var dimRectangle = new Rectangle(0f, 0f, bgSprite.getWidth(), bgSprite.getHeight());
+        dimRectangle = new Rectangle(0f, 0f, bgSprite.getWidth(), bgSprite.getHeight());
         dimRectangle.setColor(0f, 0f, 0f, 1.0f - Config.getBackgroundBrightness());
         bgSprite.attachChild(dimRectangle);
 
@@ -948,7 +949,7 @@ public class GameScene implements IUpdateHandler, GameObjectListener,
         GameHelper.setGlobalTime(0);
 
         float effectOffset = 155 - 25;
-        breakAnimator = new BreakAnimator(this, fgScene, stat, beatmapData.general.letterboxInBreaks, bgSprite);
+        breakAnimator = new BreakAnimator(this, fgScene, stat, beatmapData.general.letterboxInBreaks, dimRectangle);
         if(!Config.isHideInGameUI()){
             scorebar = new ScoreBar(this, fgScene, stat);
             addPassiveObject(scorebar);
