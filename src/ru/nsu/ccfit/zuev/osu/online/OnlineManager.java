aad282ca6c33150feb3725f72539788c2ac46d4b
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2023-11-12 01:34:33 +0800

    Bump client version

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -22,7 +22,7 @@ public class OnlineManager {
     public static final String hostname = "droidpp.osudroid.moe";
     public static final String endpoint = "https://" + hostname + "/api/droid/";
     public static final String defaultAvatarURL = "https://osudroid.moe/user/avatar/0.png";
-    private static final String onlineVersion = "36";
+    private static final String onlineVersion = "37";
 
     public static final OkHttpClient client = new OkHttpClient();
 
