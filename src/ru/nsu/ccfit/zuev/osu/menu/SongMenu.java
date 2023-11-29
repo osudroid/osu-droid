b4034612f8fa779b136628fcd594004fde3876bd
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2023-11-29 15:16:36 +0800

    Replace another local `selectedTrack` usage

--- src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
+++ src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
@@ -1142,7 +1142,7 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
         }
         isSelectComplete = false;
         this.selectedTrack = track;
-        EdExtensionHelper.onSelectTrack(selectedTrack);
+        EdExtensionHelper.onSelectTrack(track);
         GlobalManager.getInstance().setSelectedTrack(track);
         updateInfo(track);
         board.init(track);
