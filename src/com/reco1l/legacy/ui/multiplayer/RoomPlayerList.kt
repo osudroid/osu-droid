205b66ce54bd6e360ad50cbfa9cc63be9915b1f1
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2023-12-09 21:14:20 +0800

    Don't play menu click sound when in gameplay

--- src/com/reco1l/legacy/ui/multiplayer/RoomPlayerList.kt
+++ src/com/reco1l/legacy/ui/multiplayer/RoomPlayerList.kt
@@ -12,6 +12,7 @@ import org.anddev.andengine.input.touch.TouchEvent
 import org.anddev.andengine.input.touch.detector.ScrollDetector.IScrollDetectorListener
 import org.anddev.andengine.util.MathUtils
 import ru.nsu.ccfit.zuev.osu.Config
+import ru.nsu.ccfit.zuev.osu.GlobalManager.getInstance as getGlobal
 import ru.nsu.ccfit.zuev.osu.ResourceManager.getInstance as getResources
 
 class RoomPlayerList(val room: Room) : ScrollableList(), IScrollDetectorListener
@@ -182,7 +183,9 @@ class RoomPlayerList(val room: Room) : ScrollableList(), IScrollDetectorListener
                 if (moved || isScroll)
                     return true
 
-                getResources().getSound("menuclick")?.play()
+                // Only play the click sound when not in game scene. Not really an elegant solution, but works for now.
+                if (getGlobal().engine.scene != getGlobal().gameScene)
+                    getResources().getSound("menuclick")?.play()
 
                 if (player != null && Multiplayer.player != player)
                 {
