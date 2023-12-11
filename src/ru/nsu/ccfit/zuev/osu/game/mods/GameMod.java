3bde92ff18edcb3dcafcf810dbf51804cc304af9
Reco1l <geronimoferruccio15@gmail.com>
2023-12-11 20:03:06 -0300

    Code quality: Fix GameMod entries wrapping.

--- src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
+++ src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
@@ -1,11 +1,23 @@
 package ru.nsu.ccfit.zuev.osu.game.mods;
 
 public enum GameMod {
-    /*
-     * Temporarily unranked mods (until adjusted):
-     * sd, pf, sc, re, fl
-     */
-    MOD_NOFAIL(0.5f), MOD_AUTO(1, true), MOD_EASY(0.5f), MOD_HARDROCK(1.06f), MOD_HIDDEN(1.06f), MOD_RELAX(0.001f, true), MOD_AUTOPILOT(0.001f, true), MOD_DOUBLETIME(1.12f), MOD_NIGHTCORE(1.12f), MOD_HALFTIME(0.3f), MOD_SUDDENDEATH(1), MOD_PERFECT(1), MOD_FLASHLIGHT(1.12f), MOD_PRECISE(1.06f), MOD_REALLYEASY(0.5f, true), MOD_SCOREV2(1, true);
+
+    MOD_NOFAIL(0.5f),
+    MOD_AUTO(1, true),
+    MOD_EASY(0.5f),
+    MOD_HARDROCK(1.06f),
+    MOD_HIDDEN(1.06f),
+    MOD_RELAX(0.001f, true),
+    MOD_AUTOPILOT(0.001f, true),
+    MOD_DOUBLETIME(1.12f),
+    MOD_NIGHTCORE(1.12f),
+    MOD_HALFTIME(0.3f),
+    MOD_SUDDENDEATH(1),
+    MOD_PERFECT(1),
+    MOD_FLASHLIGHT(1.12f),
+    MOD_PRECISE(1.06f),
+    MOD_REALLYEASY(0.5f, true),
+    MOD_SCOREV2(1, true);
 
     public final float scoreMultiplier;
 
