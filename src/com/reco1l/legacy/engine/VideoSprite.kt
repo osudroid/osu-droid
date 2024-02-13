8a416e21d28d562f7ab6dfec1d3e8d550c329b8f
ammarasyad <ammarasyad@gmail.com>
2024-02-13 15:22:56 +0700

    Add a comment for clarity

--- src/com/reco1l/legacy/engine/VideoSprite.kt
+++ src/com/reco1l/legacy/engine/VideoSprite.kt
@@ -27,6 +27,8 @@ class VideoSprite(source: String, private val engine: Engine) : Sprite(0f, 0f, V
     {
         onInitDraw(pGL)
 
+        // Apparently there is either a bug or unintended behavior in Mali GPUs' OpenGL ES implementation.
+        // Causes the wrong texture to be displayed when GL_TEXTURE_2D is enabled before enabling GL_TEXTURE_EXTERNAL_OES.
         if (pGL.glGetString(GL10.GL_RENDERER).contains("Mali", true)) {
             pGL.glDisable(GL_TEXTURE_2D)
         }
