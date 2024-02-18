8877ed851b4fa9c3c135ac5397fb9d83a4e30dff
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-02-18 08:04:00 +0700

    Add circle size converter

---
+++ src/com/rian/osu/utils/CircleSizeCalculator.kt
@@ -0,0 +1,77 @@
+package com.rian.osu.utils
+
+import com.rian.osu.GameMode
+import com.rian.osu.beatmap.hitobject.HitObject
+import kotlin.math.max
+
+/**
+ * A utility for converting circle sizes across [GameMode]s.
+ */
+object CircleSizeCalculator {
+    // This is not the real height that is used in the game, but rather an assumption so that we can treat
+    // circle sizes similarly across all devices.
+    private const val ASSUMED_DROID_HEIGHT = 681
+
+    /**
+     * Converts osu!droid circle size to osu!droid scale.
+     *
+     * @param cs The circle size to convert.
+     * @return The calculated osu!droid scale.
+     */
+    fun droidCSToDroidScale(cs: Float) =
+        max(1e-3f, ASSUMED_DROID_HEIGHT / 480 * (54.42f - cs * 4.48f) * 2 / 128 + ((0.5 * (11 - 5.2450170716245195)) / 5).toFloat())
+
+    /**
+     * Converts osu!droid scale to osu!standard radius.
+     *
+     * @param scale The osu!droid scale to convert.
+     * @return The osu!standard scale of the given radius.
+     */
+    fun droidScaleToStandardRadius(scale: Float) =
+        HitObject.OBJECT_RADIUS * max(1e-3f, scale) / (ASSUMED_DROID_HEIGHT * 0.85f / 384)
+
+    /**
+     * Converts osu!standard radius to osu!droid scale.
+     *
+     * @param radius The osu!standard radius to convert.
+     * @return The osu!droid scale of the given osu!standard radius.
+     */
+    fun standardRadiusToDroidScale(radius: Double) = max(1e-3f,
+        radius.toFloat() * ASSUMED_DROID_HEIGHT * 0.85f / 384 / HitObject.OBJECT_RADIUS)
+
+    /**
+     * Converts osu!standard radius to osu!standard circle size.
+     *
+     * @param radius The osu!standard radius to convert.
+     * @return The osu!standard circle size at the given radius.
+     */
+    fun standardRadiusToStandardCS(radius: Double) =
+        5 + ((1 - radius.toFloat() / (HitObject.OBJECT_RADIUS / 2)) * 5) / 0.7f
+
+    /**
+     * Converts osu!standard circle size to osu!standard scale.
+     *
+     * @param cs The osu!standard circle size to convert.
+     * @return The osu!standard scale of the given circle size.
+     */
+    fun standardCSToStandardScale(cs: Float) =
+        // The following comment is copied verbatim from osu!lazer and osu!stable:
+        //
+        //   Builds of osu! up to 2013-05-04 had the gamefield being rounded down, which caused incorrect radius calculations
+        //   in widescreen cases. This ratio adjusts to allow for old replays to work post-fix, which in turn increases the lenience
+        //   for all plays, but by an amount so small it should only be effective in replays.
+        //
+        // To match expectations of gameplay we need to apply this multiplier to circle scale. It's weird but is what it is.
+        // It works out to under 1 game pixel and is generally not meaningful to gameplay, but is to replay playback accuracy.
+        //
+        // This scale affects difficulty calculation by a bit, hence it is also put here despite not being relevant to replays.
+        (1 - 0.7f * (cs - 5) / 5) / 2 * 1.00041f
+
+    /**
+     * Converts osu!standard scale to osu!droid scale.
+     *
+     * @param scale The osu!standard scale to convert.
+     * @return The osu!droid scale of the given osu!standard scale.
+     */
+    fun standardScaleToDroidScale(scale: Float) = standardRadiusToDroidScale(HitObject.OBJECT_RADIUS * scale.toDouble())
+}
