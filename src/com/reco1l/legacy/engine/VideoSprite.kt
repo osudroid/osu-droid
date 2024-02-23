ee9a6a86421b9061d4e81d27ecb68b223736a7af
Reco1l <geronimoferruccio15@gmail.com>
2024-02-23 16:20:38 -0300

    Fix video not being shown.

--- src/com/reco1l/legacy/engine/VideoSprite.kt
+++ src/com/reco1l/legacy/engine/VideoSprite.kt
@@ -6,9 +6,7 @@ import com.reco1l.legacy.graphics.ExternalTextureShaderProgram
 import org.andengine.engine.Engine
 import org.andengine.engine.camera.Camera
 import org.andengine.entity.sprite.Sprite
-import org.andengine.opengl.texture.region.TextureRegion
 import org.andengine.opengl.util.GLState
-import ru.nsu.ccfit.zuev.osu.GlobalManager
 
 class VideoSprite(source: String, engine: Engine) : Sprite(
     0f,
@@ -21,13 +19,16 @@ class VideoSprite(source: String, engine: Engine) : Sprite(
 
     val texture = textureRegion.texture as VideoTexture
 
+    init
+    {
+        texture.load()
+    }
+
 
     override fun preDraw(pGLState: GLState, pCamera: Camera)
     {
         // Since video dimensions are 0x0 initially due to the surface not being set yet, we have to
         // ensure once the surface is set the texture coordinates are updated.
-        textureRegion.set(0f, 0f, texture.width.toFloat(), texture.height.toFloat())
-        (textureRegion as TextureRegion).updateUV()
 
         GLES20.glEnable(GL_TEXTURE_EXTERNAL_OES)
         super.preDraw(pGLState, pCamera)
@@ -42,9 +43,11 @@ class VideoSprite(source: String, engine: Engine) : Sprite(
 
     override fun dispose()
     {
-        if (isDisposed)
+        if (!isDisposed)
+        {
             texture.release()
-
+            texture.unload()
+        }
         super.dispose()
     }
 }
