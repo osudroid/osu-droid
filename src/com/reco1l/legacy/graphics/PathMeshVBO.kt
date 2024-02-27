17e15a75c89eb7d72bfc4756f9b2ce3c7e7e60c4
Reco1l <geronimoferruccio15@gmail.com>
2024-02-27 20:15:29 -0300

    Update alpha through a shader instead of refill buffers.

--- src/com/reco1l/legacy/graphics/PathMeshVBO.kt
+++ src/com/reco1l/legacy/graphics/PathMeshVBO.kt
@@ -3,9 +3,10 @@ package com.reco1l.legacy.graphics
 import android.opengl.GLES20
 import org.andengine.entity.primitive.Mesh
 import org.andengine.entity.primitive.vbo.IMeshVertexBufferObject
+import org.andengine.opengl.shader.ShaderProgram
+import org.andengine.opengl.util.GLState
 import org.andengine.opengl.vbo.DrawType
 import org.andengine.opengl.vbo.VertexBufferObject
-import ru.nsu.ccfit.zuev.osu.GlobalManager
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -30,16 +31,8 @@ class PathMeshVBO(private val flat: Boolean) :
             setDirtyOnHardware()
         }
 
-    var alpha = 0f
-        set(value)
-        {
-            if (field != value)
-            {
-                field = value
-                setDirtyOnHardware()
-            }
-        }
 
+    private var alpha = 0f
 
     private var floatBuffer = mByteBuffer.asFloatBuffer()
 
@@ -52,6 +45,15 @@ class PathMeshVBO(private val flat: Boolean) :
     override fun getNativeHeapMemoryByteSize() = byteCapacity
 
 
+    override fun bind(gl: GLState, shader: ShaderProgram)
+    {
+        super.bind(gl, AlphaOverrideShaderProgram)
+
+        AlphaOverrideShaderProgram.setAlphaUniform(alpha)
+    }
+
+    override fun unbind(gl: GLState, shader: ShaderProgram) = super.unbind(gl, AlphaOverrideShaderProgram)
+
 
     override fun onUpdateVertices(mesh: Mesh) = setDirtyOnHardware()
 
@@ -62,77 +64,10 @@ class PathMeshVBO(private val flat: Boolean) :
         alpha = mesh.alpha
     }
 
-
-    private fun repackColor(rawColor: Float, alpha: Int): Float
-    {
-        val colorBits = rawColor.toRawBits()
-
-        // Source color with alpha applied.
-        val packedBits = (alpha shl 24)
-            .or(colorBits shr 16 and 0xFF shl 16)
-            .or(colorBits shr 8 and 0xFF shl 8)
-            .or(colorBits shr 0 and 0xFF shl 0)
-
-        return Float.fromBits(packedBits and -0x1)
-    }
-
-
-    private fun onBufferColor(buffer: FloatArray)
-    {
-        // This will only update the alpha since is expected to the color be specified at every
-        // vertex due to the attributes.
-        val alpha = (255f * alpha).toInt()
-
-        var firstColor = Float.NaN
-        var secondColor = Float.NaN
-
-        var firstColorComputed = 0f
-        var secondColorComputed = 0f
-
-        // Bits operations punish performance specially fromBits() and toRawBits() functions.
-        // Here we try to do these operations once, and then reuse the results.
-
-        for (i in 0 until buffer.size / if (flat) 3 else 4)
-        {
-            val index = if (flat)
-                i * 3 + 2 // 2D vertices
-            else
-                i * 4 + 3 // 3D vertices
-
-            val packedColor = buffer[index]
-
-            if (packedColor != firstColor && packedColor != secondColor)
-            {
-                if (firstColor.isNaN())
-                {
-                    firstColor = packedColor
-                    firstColorComputed = repackColor(packedColor, alpha)
-                }
-                else if (secondColor.isNaN())
-                {
-                    secondColor = packedColor
-                    secondColorComputed = repackColor(packedColor, alpha)
-                }
-            }
-
-            buffer[index] = when (packedColor)
-            {
-                firstColor -> firstColorComputed
-                secondColor -> secondColorComputed
-
-                // Should not be reached since this is intended to be used with two colors only.
-                else -> repackColor(packedColor, alpha)
-            }
-        }
-    }
-
-
     override fun onBufferData()
     {
         val buffer = buffer
         mCapacity = buffer.size
-
-        onBufferColor(buffer)
 
         if (floatBuffer.capacity() < mCapacity)
         {
--- src/com/reco1l/legacy/graphics/Shaders.kt
+++ src/com/reco1l/legacy/graphics/Shaders.kt
@@ -1,10 +1,21 @@
 package com.reco1l.legacy.graphics
 
 import android.opengl.GLES20
+import org.andengine.opengl.shader.PositionColorShaderProgram
 import org.andengine.opengl.shader.PositionColorTextureCoordinatesShaderProgram
-import org.andengine.opengl.shader.PositionColorTextureCoordinatesShaderProgram.*
+import org.andengine.opengl.shader.PositionColorTextureCoordinatesShaderProgram.FRAGMENTSHADER
+import org.andengine.opengl.shader.PositionColorTextureCoordinatesShaderProgram.VERTEXSHADER
 import org.andengine.opengl.shader.ShaderProgram
-import org.andengine.opengl.shader.constants.ShaderProgramConstants.*
+import org.andengine.opengl.shader.constants.ShaderProgramConstants
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_COLOR
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_COLOR_LOCATION
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_POSITION
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_POSITION_LOCATION
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_TEXTURECOORDINATES
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.ATTRIBUTE_TEXTURECOORDINATES_LOCATION
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.LOCATION_INVALID
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.UNIFORM_MODELVIEWPROJECTIONMATRIX
+import org.andengine.opengl.shader.constants.ShaderProgramConstants.UNIFORM_TEXTURE_0
 import org.andengine.opengl.util.GLState
 import org.andengine.opengl.vbo.attribute.VertexBufferObjectAttributes
 
@@ -20,6 +31,10 @@ object ExternalTextureShaderProgram : ShaderProgram(
     "#extension GL_OES_EGL_image_external : require\n" + FRAGMENTSHADER.replace("sampler2D", "samplerExternalOES"),
 )
 {
+
+    private var sUniformModelViewPositionMatrixLocation = LOCATION_INVALID
+    private var sUniformTexture0Location = LOCATION_INVALID
+
 
     override fun link(gl: GLState)
     {
@@ -41,4 +56,57 @@ object ExternalTextureShaderProgram : ShaderProgram(
         GLES20.glUniform1i(sUniformTexture0Location, 0)
     }
 
+}
+
+
+/**
+ * Same as [PositionColorShaderProgram] but allows to override fragments alpha via an uniform.
+ */
+object AlphaOverrideShaderProgram : ShaderProgram(
+    PositionColorShaderProgram.VERTEXSHADER,
+    """
+        precision lowp float;
+        varying vec4 ${ShaderProgramConstants.VARYING_COLOR};
+        uniform float ${ShaderProgramConstants.UNIFORM_ALPHA};
+        
+        void main() {
+            vec4 color = ${ShaderProgramConstants.VARYING_COLOR};
+            color.a = ${ShaderProgramConstants.UNIFORM_ALPHA};
+            gl_FragColor = color;
+        }  
+    """.trimIndent()
+)
+{
+
+    private var sUniformModelViewPositionMatrixLocation = LOCATION_INVALID
+    private var sUniformAlphaLocation = LOCATION_INVALID
+
+
+    override fun link(gl: GLState)
+    {
+        GLES20.glBindAttribLocation(mProgramID, ATTRIBUTE_POSITION_LOCATION, ATTRIBUTE_POSITION)
+        GLES20.glBindAttribLocation(mProgramID, ATTRIBUTE_COLOR_LOCATION, ATTRIBUTE_COLOR)
+
+        super.link(gl)
+
+        sUniformModelViewPositionMatrixLocation = getUniformLocation(UNIFORM_MODELVIEWPROJECTIONMATRIX)
+        sUniformAlphaLocation = getUniformLocation(ShaderProgramConstants.UNIFORM_ALPHA)
+    }
+
+    override fun bind(gl: GLState, pVertexBufferObjectAttributes: VertexBufferObjectAttributes)
+    {
+        GLES20.glDisableVertexAttribArray(ATTRIBUTE_TEXTURECOORDINATES_LOCATION)
+
+        super.bind(gl, pVertexBufferObjectAttributes)
+
+        GLES20.glUniformMatrix4fv(sUniformModelViewPositionMatrixLocation, 1, false, gl.getModelViewProjectionGLMatrix(), 0)
+    }
+
+    override fun unbind(gl: GLState)
+    {
+        GLES20.glEnableVertexAttribArray(ATTRIBUTE_TEXTURECOORDINATES_LOCATION)
+        super.unbind(gl)
+    }
+
+    fun setAlphaUniform(alpha: Float) = GLES20.glUniform1f(sUniformAlphaLocation, alpha)
 }
