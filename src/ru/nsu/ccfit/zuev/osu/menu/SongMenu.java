2ba7aace02ab55bf0769a65c79a08f88a3878bf8
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-03-21 22:32:06 +0800

    Set scoring switcher to "not found" frame before retrieving ranked status
    (cherry picked from commit 360e7d370c5eafbf6c20b37b892b7863b3b8bddd)

--- src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
+++ src/ru/nsu/ccfit/zuev/osu/menu/SongMenu.java
@@ -1528,6 +1528,7 @@ public class SongMenu implements IUpdateHandler, MenuItemListener,
         }
 
         var hash = selectedTrack.getMD5();
+        scoringSwitcher.setFrame(0);
 
         Execution.async(() -> {
             try {
