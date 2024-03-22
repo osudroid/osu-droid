fd83df124a5d9ee4f6329126b5cbe13c2c4276ac
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-03-22 11:38:13 +0800

    Add ranked status parsing from int

--- src/ru/nsu/ccfit/zuev/osu/RankedStatus.java
+++ src/ru/nsu/ccfit/zuev/osu/RankedStatus.java
@@ -7,5 +7,18 @@ public enum RankedStatus {
     loved,
     pending,
     workInProgress,
-    graveyard
+    graveyard;
+
+    public static RankedStatus valueOf(int value) {
+        return switch (value) {
+            case 1 -> ranked;
+            case 2 -> approved;
+            case 3 -> qualified;
+            case 4 -> loved;
+            case 0 -> pending;
+            case -1 -> workInProgress;
+            case -2 -> graveyard;
+            default -> throw new IllegalArgumentException("Invalid value: " + value);
+        };
+    }
 }
