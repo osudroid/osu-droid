e575a930cbc7e0ee127f5cc0a8d745e2cde87f33
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-08-18 11:14:06 +0700

    Move playable beatmap creation from `DifficultyCalculator` directly to `Beatmap`

--- src/com/rian/osu/beatmap/Beatmap.kt
+++ src/com/rian/osu/beatmap/Beatmap.kt
@@ -1,8 +1,15 @@
 package com.rian.osu.beatmap
 
+import com.rian.osu.GameMode
+import com.rian.osu.beatmap.hitobject.HitObject
 import com.rian.osu.beatmap.hitobject.Slider
 import com.rian.osu.beatmap.hitobject.getEndTime
 import com.rian.osu.beatmap.sections.*
+import com.rian.osu.mods.IApplicableToBeatmap
+import com.rian.osu.mods.IApplicableToDifficulty
+import com.rian.osu.mods.IApplicableToDifficultyWithSettings
+import com.rian.osu.mods.IApplicableToHitObject
+import com.rian.osu.mods.Mod
 
 /**
  * Represents a beatmap.
@@ -118,6 +125,57 @@ class Beatmap : Cloneable {
      */
     val duration: Int
         get() = hitObjects.objects.lastOrNull()?.getEndTime()?.toInt() ?: 0
+
+    /**
+     * Constructs a playable [Beatmap] from this [Beatmap].
+     *
+     * The returned [Beatmap] is in a playable state - all [HitObject] and [BeatmapDifficulty] [Mod]s have been applied,
+     * and [HitObject]s have been fully constructed.
+     *
+     * @param mode The [GameMode] to construct the [Beatmap] for.
+     * @param mods The [Mod]s to apply to the [Beatmap]. Defaults to No Mod.
+     * @param customSpeedMultiplier The custom speed multiplier to apply to the [Beatmap]. Defaults to 1.
+     * @return The constructed [Beatmap].
+     */
+    @JvmOverloads
+    fun createPlayableBeatmap(mode: GameMode, mods: List<Mod>? = null, customSpeedMultiplier: Float = 1f): Beatmap {
+        val converter = BeatmapConverter(this)
+
+        // Convert
+        val converted = converter.convert()
+
+        // Apply difficulty mods
+        mods?.filterIsInstance<IApplicableToDifficulty>()?.forEach {
+            it.applyToDifficulty(mode, converted.difficulty)
+        }
+
+        mods?.filterIsInstance<IApplicableToDifficultyWithSettings>()?.forEach {
+            it.applyToDifficulty(mode, converted.difficulty, mods, customSpeedMultiplier)
+        }
+
+        val processor = BeatmapProcessor(converted)
+
+        processor.preProcess()
+
+        // Compute default values for hit objects, including creating nested hit objects in-case they're needed
+        converted.hitObjects.objects.forEach {
+            it.applyDefaults(converted.controlPoints, converted.difficulty, mode)
+        }
+
+        mods?.filterIsInstance<IApplicableToHitObject>()?.forEach {
+            for (obj in converted.hitObjects.objects) {
+                it.applyToHitObject(mode, obj)
+            }
+        }
+
+        processor.postProcess(mode)
+
+        mods?.filterIsInstance<IApplicableToBeatmap>()?.forEach {
+            it.applyToBeatmap(converted)
+        }
+
+        return converted
+    }
 
     public override fun clone() = super.clone() as Beatmap
 }
