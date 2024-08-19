f822e2651848669800c3069594deedd4defde6f1
Gerónimo Carlos Ferruccio <geronimoferruccio15@gmail.com>
2024-08-19 09:22:10 -0300

    Reference class name rather than using an string

--- src/com/reco1l/osu/Database.kt
+++ src/com/reco1l/osu/Database.kt
@@ -29,7 +29,7 @@ object DatabaseManager {
     fun load(context: Context) {
 
         // Be careful when changing the database name, it may cause data loss.
-        database = Room.databaseBuilder(context, DroidDatabase::class.java, "DroidDatabase")
+        database = Room.databaseBuilder(context, DroidDatabase::class.java, DroidDatabase::class.simpleName)
             // Is preferable to support migrations, otherwise destructive migration will run forcing
             // tables to recreate (in case of beatmaps table it'll re-import all beatmaps).
             // See https://developer.android.com/training/data-storage/room/migrating-db-versions.
