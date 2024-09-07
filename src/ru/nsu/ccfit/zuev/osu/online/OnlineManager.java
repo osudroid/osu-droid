fc46208a7a8f9b9b07949a3d5ff36a0a95239de4
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-09-07 13:24:58 +0700

    Add global beatmap pp leaderboard

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -28,6 +28,7 @@ import org.json.JSONObject;
 import ru.nsu.ccfit.zuev.osu.*;
 import ru.nsu.ccfit.zuev.osu.helper.MD5Calculator;
 import ru.nsu.ccfit.zuev.osu.online.PostBuilder.RequestException;
+import ru.nsu.ccfit.zuev.osu.scoring.BeatmapLeaderboardScoringMode;
 
 public class OnlineManager {
     public static final String hostname = "osudroid.moe";
@@ -292,6 +293,12 @@ public class OnlineManager {
         post.addParam("filename", beatmapFile.getName());
         post.addParam("hash", hash);
         post.addParam("uid", String.valueOf(userId));
+
+        if (Config.getBeatmapLeaderboardScoringMode() == BeatmapLeaderboardScoringMode.PP) {
+            post.addParam("type", "pp");
+        } else {
+            post.addParam("type", "score");
+        }
 
         ArrayList<String> response = sendRequest(post, endpoint + "getrank.php");
 
