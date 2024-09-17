7f66f0bd38988aad625dd8c07a70705a2f88d042
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-09-17 10:52:39 +0700

    Add `Vector2` linear interpolation method

--- src/com/rian/osu/math/Interpolation.kt
+++ src/com/rian/osu/math/Interpolation.kt
@@ -25,4 +25,18 @@ object Interpolation {
      */
     @JvmStatic
     fun linear(start: Float, end: Float, amount: Float) = start + (end - start) * amount
+
+    /**
+     * Performs a linear interpolation.
+     *
+     * @param start The starting point of the interpolation.
+     * @param end The final point of the interpolation.
+     * @param amount The interpolation multiplier.
+     * @return The interpolated value.
+     */
+    @JvmStatic
+    fun linear(start: Vector2, end: Vector2, amount: Float) = Vector2(
+        linear(start.x, end.x, amount),
+        linear(start.y, end.y, amount)
+    )
 }
