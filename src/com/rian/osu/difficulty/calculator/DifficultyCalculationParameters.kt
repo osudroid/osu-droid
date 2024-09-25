20bae1f1bb55b4d200cfa40f073c690f8929663a
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-09-25 09:59:59 +0700

    Use lazy property for `totalSpeedMultiplier`

--- src/com/rian/osu/difficulty/calculator/DifficultyCalculationParameters.kt
+++ src/com/rian/osu/difficulty/calculator/DifficultyCalculationParameters.kt
@@ -1,5 +1,6 @@
 package com.rian.osu.difficulty.calculator
 
+import com.rian.osu.mods.IApplicableToTrackRate
 import com.rian.osu.mods.Mod
 import com.rian.osu.mods.ModDoubleTime
 import com.rian.osu.mods.ModHalfTime
@@ -22,20 +23,11 @@ class DifficultyCalculationParameters {
     /**
      * The overall speed multiplier to calculate for.
      */
-    val totalSpeedMultiplier: Float
-        get() {
-            var speedMultiplier = customSpeedMultiplier
-
-            if (mods.any { it is ModDoubleTime || it is ModNightCore }) {
-                speedMultiplier *= 1.5f
-            }
-
-            if (mods.any { it is ModHalfTime }) {
-                speedMultiplier *= 0.75f
-            }
-
-            return speedMultiplier
+    val totalSpeedMultiplier by lazy {
+        customSpeedMultiplier * mods.filterIsInstance<IApplicableToTrackRate>().fold(1f) { acc, mod ->
+            acc * mod.trackRateMultiplier
         }
+    }
 
     /**
      * Copies this instance to another instance.
