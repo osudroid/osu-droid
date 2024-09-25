85efe6d3aabd1e9dd794fd57852a3f1401c36c66
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-09-25 16:36:36 +0700

    Optimize `FollowPointConnection` by early returning if there are no follow points to draw

--- src/com/reco1l/osu/playfield/FollowPointConnection.kt
+++ src/com/reco1l/osu/playfield/FollowPointConnection.kt
@@ -24,7 +24,7 @@ object FollowPointConnection {
     @JvmStatic
     val pool = Pool {
 
-        // For optimization we avoid to use AnimatedSprite if there's one frame.
+        // For optimization, we avoid using AnimatedSprite if there's one frame.
         if (ResourceManager.getInstance().isTextureLoaded("followpoint-0")) {
             AnimatedSprite("followpoint", true, OsuSkin.get().animationFramerate)
         } else {
@@ -45,8 +45,25 @@ object FollowPointConnection {
 
     @JvmStatic
     fun addConnection(scene: Scene, secPassed: Float, start: HitObject, end: HitObject) {
-
         // Reference: https://github.com/ppy/osu/blob/7bc8908ca9c026fed1d831eb6e58df7624a8d614/osu.Game.Rulesets.Osu/Objects/Drawables/Connections/FollowPointConnection.cs
+
+        // Since the unit of spacing is in osu!pixels, we cannot directly port the reference code. As such, we need to
+        // approach it with another method. We use the distance between the start and end positions in osu!pixels to
+        // determine the amount of sprites to spawn, and then map them into gameplay positions in pixels.
+        val osuPixelsStartPosition = start.difficultyStackedEndPosition
+        val osuPixelsEndPosition = end.difficultyStackedPosition
+
+        val osuPixelsDistance = hypot(
+            osuPixelsEndPosition.x - osuPixelsStartPosition.x,
+            osuPixelsEndPosition.y - osuPixelsStartPosition.y
+        )
+
+        val spriteCount = floor(osuPixelsDistance / SPACING).toInt()
+
+        if (spriteCount == 0) {
+            return
+        }
+
         val scale = start.gameplayScale
         val startTime = (start.endTime / 1000f).toFloat()
 
@@ -65,19 +82,6 @@ object FollowPointConnection {
         // This uniform speedup is hard to match 1:1, however we can at least make AR>10 (via mods) feel good by extending the upper linear preempt function.
         // Note that this doesn't exactly match the AR>10 visuals as they're classically known, but it feels good.
         val preempt = PREEMPT * min(1.0, start.timePreempt / HitObject.PREEMPT_MIN).toFloat() / 1000f
-
-        // Since the unit of spacing is in osu!pixels, we cannot directly port the reference code. As such, we need to
-        // approach it with another method. We use the distance between the start and end positions in osu!pixels to
-        // determine the amount of sprites to spawn, and then map them into gameplay positions in pixels.
-        val osuPixelsStartPosition = start.difficultyStackedEndPosition
-        val osuPixelsEndPosition = end.difficultyStackedPosition
-
-        val osuPixelsDistance = hypot(
-            osuPixelsEndPosition.x - osuPixelsStartPosition.x,
-            osuPixelsEndPosition.y - osuPixelsStartPosition.y
-        )
-
-        val spriteCount = floor(osuPixelsDistance / SPACING).toInt()
         val pointStartOffset = 0.1f / spriteCount
 
         for (i in 1 ..spriteCount) {
