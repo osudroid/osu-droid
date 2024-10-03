91de01202cf78405266b70382f4d03dd221e6178
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-10-03 22:57:17 +0800

    Add `followDelay` property to `ModFlashlight`

--- src/com/rian/osu/mods/ModFlashlight.kt
+++ src/com/rian/osu/mods/ModFlashlight.kt
@@ -11,8 +11,27 @@ class ModFlashlight : Mod(), IModUserSelectable {
     override val textureNameSuffix = "flashlight"
     override val ranked = true
 
+    /**
+     * The amount of seconds until the flashlight reaches the cursor.
+     */
+    var followDelay = DEFAULT_FOLLOW_DELAY
+
     override fun calculateScoreMultiplier(difficulty: BeatmapDifficulty) = 1.12f
 
     override fun equals(other: Any?) = other === this || other is ModFlashlight
-    override fun hashCode() = super.hashCode()
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+
+        result = 31 * result + followDelay.hashCode()
+
+        return result
+    }
+
+    companion object {
+        /**
+         * The default amount of seconds until the flashlight reaches the cursor.
+         */
+        const val DEFAULT_FOLLOW_DELAY = 0.12f
+    }
 }
