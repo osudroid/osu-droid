d869114e4e4483c79b1f2af6d72a3340d7380048
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-10-10 19:41:25 +0800

    Include sliders as objects with accuracy for ScoreV2 scores when estimating deviation in dpp calculation

--- src/com/rian/osu/difficulty/calculator/DroidPerformanceCalculator.kt
+++ src/com/rian/osu/difficulty/calculator/DroidPerformanceCalculator.kt
@@ -309,9 +309,9 @@ class DroidPerformanceCalculator(
      * The estimation is consistent in that two SS scores on the same map
      * with the same settings will always return the same deviation.
      *
-     * Sliders are treated as circles with a 50 hit window.
+     * Under non-ScoreV2 scores, sliders are treated as circles with a 50 hit window.
      *
-     * Misses are ignored because they are usually due to misaiming, and 50s
+     * Misses are ignored because they are usually due to mis-aiming, and 50s
      * are grouped with 100s since they are usually due to misreading.
      *
      * Inaccuracies are capped to the number of circles in the map.
@@ -336,42 +336,51 @@ class DroidPerformanceCalculator(
         val okWindow = hitWindow.okWindow / clockRate
         val mehWindow = hitWindow.mehWindow / clockRate
 
-        val missCountCircles = min(countMiss, hitCircleCount)
-        val mehCountCircles = min(countMeh, hitCircleCount - missCountCircles)
-        val okCountCircles = min(countOk, hitCircleCount - missCountCircles - mehCountCircles)
-        val greatCountCircles = max(0, hitCircleCount - missCountCircles - mehCountCircles - okCountCircles)
+        // For non-ScoreV2 scores, assume 100s, 50s, and misses happen on circles.
+        // If there are less non-300s on circles than 300s, compute the deviation on circles.
+        val isScoreV2 = mods.any { it is ModScoreV2 }
+        val hitObjectWithAccuracyCount = hitCircleCount + if (isScoreV2) sliderCount else 0
 
-        // Assume 100s, 50s, and misses happen on circles. If there are less non-300s on circles than 300s,
-        // compute the deviation on circles.
-        if (greatCountCircles > 0) {
-            // The probability that a player hits a circle is unknown, but we can estimate it to be
+        val missCountAccuracyObjects = min(countMiss, hitObjectWithAccuracyCount)
+        val mehCountAccuracyObjects = min(countMeh, hitObjectWithAccuracyCount - missCountAccuracyObjects)
+        val okCountAccuracyObjects = min(countOk, hitObjectWithAccuracyCount - missCountAccuracyObjects - mehCountAccuracyObjects)
+        val greatCountAccuracyObjects = max(0, hitObjectWithAccuracyCount - missCountAccuracyObjects - mehCountAccuracyObjects - okCountAccuracyObjects)
+
+        if (greatCountAccuracyObjects > 0) {
+            // The probability that a player hits an accuracy object is unknown, but we can estimate it to be
             // the number of greats on circles divided by the number of circles, and then add one
             // to the number of circles as a bias correction / bayesian prior.
-            val greatProbabilityCircle =
-                greatCountCircles / (hitCircleCount - missCountCircles - mehCountCircles + 1.0)
+            val greatProbabilityAccuracyObjects =
+                greatCountAccuracyObjects / (hitObjectWithAccuracyCount - missCountAccuracyObjects - mehCountAccuracyObjects + 1.0)
 
             // Compute the deviation assuming 300s and 100s are normally distributed, and 50s are uniformly distributed.
             // Begin with the normal distribution first.
-            var deviationOnCircles = greatWindow / (sqrt(2.0) * ErrorFunction.erfInv(greatProbabilityCircle))
+            var deviationOnAccuracyObjects = greatWindow / (sqrt(2.0) * ErrorFunction.erfInv(greatProbabilityAccuracyObjects))
 
-            deviationOnCircles *=
-                sqrt(1 - sqrt(2 / PI) * okWindow * exp(-0.5 * (okWindow / deviationOnCircles).pow(2)) /
-                    (deviationOnCircles * ErrorFunction.erf(okWindow / (sqrt(2.0) * deviationOnCircles))))
+            deviationOnAccuracyObjects *=
+                sqrt(1 - sqrt(2 / PI) * okWindow * exp(-0.5 * (okWindow / deviationOnAccuracyObjects).pow(2)) /
+                    (deviationOnAccuracyObjects * ErrorFunction.erf(okWindow / (sqrt(2.0) * deviationOnAccuracyObjects))))
 
             // Then compute the variance for 50s.
             val mehVariance = (mehWindow.pow(2) + mehWindow * okWindow + okWindow.pow(2)) / 3
 
             // Find the total deviation.
             return@run sqrt(
-                ((greatCountCircles + okCountCircles) * deviationOnCircles.pow(2) + mehCountCircles * mehVariance) /
-                    (greatCountCircles + okCountCircles + mehCountCircles)
+                ((greatCountAccuracyObjects + okCountAccuracyObjects) * deviationOnAccuracyObjects.pow(2) + mehCountAccuracyObjects * mehVariance) /
+                    (greatCountAccuracyObjects + okCountAccuracyObjects + mehCountAccuracyObjects)
             )
         }
 
         // If there are more non-300s than there are circles, compute the deviation on sliders instead.
         // Here, all that matters is whether the slider was missed, since it is impossible
         // to get a 100 or 50 on a slider by mis-tapping it.
-        val missCountSliders = min(sliderCount, countMiss - missCountCircles)
+
+        // For ScoreV2 scores, sliders are already included as accuracy objects, so this part of the computation is invalid.
+        if (isScoreV2) {
+            return@run Double.POSITIVE_INFINITY
+        }
+
+        val missCountSliders = min(sliderCount, countMiss - missCountAccuracyObjects)
         val greatCountSliders = sliderCount - missCountSliders
 
         // We only get here if nothing was hit. In this case, there is no estimate for deviation.
