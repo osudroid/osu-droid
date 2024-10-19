ae2590fdbf64564bc9b055b656398c1b956c89b8
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-10-19 08:35:48 +0800

    Rename clear beatmap cache key

--- src/com/reco1l/osu/ui/SettingsFragment.kt
+++ src/com/reco1l/osu/ui/SettingsFragment.kt
@@ -251,7 +251,7 @@ class SettingsFragment : com.edlplan.ui.fragment.SettingsFragment() {
 
 
     private fun handleLibrarySectionPreferences() {
-        findPreference<Preference>("clear")!!.setOnPreferenceClickListener {
+        findPreference<Preference>("clear_beatmap_cache")!!.setOnPreferenceClickListener {
             LibraryManager.clearDatabase()
             ToastLogger.showText(StringTable.get(R.string.message_lib_cleared), true)
             true
