c7abb9e1454d307a848a82972237d83d22f07495
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-11-06 13:31:50 +0800

    Fix frame offset being applied in next update tick rather than previous

--- src/ru/nsu/ccfit/zuev/osu/game/GameplayHitCircle.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameplayHitCircle.java
@@ -180,8 +180,11 @@ public class GameplayHitCircle extends GameObject {
         scene = null;
     }
 
-    private boolean canBeHit() {
-        return passedTime >= Math.max(0, timePreempt - objectHittableRange);
+    private boolean canBeHit(float dt, float frameHitOffset) {
+        // At this point, the object's state is already in the next update tick.
+        // However, hit judgements require the object's state to be in the previous tick.
+        // Therefore, we subtract dt to get the object's state in the previous tick.
+        return passedTime - dt + frameHitOffset >= Math.max(0, timePreempt - objectHittableRange);
     }
 
     private boolean isHit() {
@@ -248,23 +251,26 @@ public class GameplayHitCircle extends GameObject {
                 removeFromScene();
                 return;
             }
-        } else if (isHit() && canBeHit()) {
-            float signAcc = passedTime - timePreempt;
-            if (Config.isFixFrameOffset()) {
-                signAcc += (float) hitOffsetToPreviousFrame() / 1000f;
+        } else {
+            float frameHitOffset = (float) hitOffsetToPreviousFrame() / 1000;
+
+            if (canBeHit(dt, frameHitOffset) && isHit()) {
+                // At this point, the object's state is already in the next update tick.
+                // However, hit judgements require the object's state to be in the previous tick.
+                // Therefore, we subtract dt to get the object's state in the previous tick.
+                float signAcc = passedTime - timePreempt - dt + frameHitOffset;
+                final float acc = Math.abs(signAcc);
+                if (acc <= GameHelper.getDifficultyHelper().hitWindowFor50(GameHelper.getOverallDifficulty())) {
+                    playHitSamples();
+                }
+                listener.registerAccuracy(signAcc);
+                passedTime = -1;
+                // Remove circle and register hit in update thread
+                startHit = true;
+                listener.onCircleHit(id, signAcc, position, endsCombo, (byte) 0, comboColor);
+                removeFromScene();
+                return;
             }
-            final float acc = Math.abs(signAcc);
-            if (acc <= GameHelper.getDifficultyHelper().hitWindowFor50(GameHelper.getOverallDifficulty())) {
-                playHitSamples();
-            }
-            listener.registerAccuracy(signAcc);
-            passedTime = -1;
-            // Remove circle and register hit in update thread
-            float finalSignAcc = signAcc;
-            startHit = true;
-            listener.onCircleHit(id, finalSignAcc, position, endsCombo, (byte) 0, comboColor);
-            removeFromScene();
-            return;
         }
 
         if (GameHelper.isKiai()) {
@@ -309,11 +315,13 @@ public class GameplayHitCircle extends GameObject {
 
     @Override
     public void tryHit(final float dt) {
-        if (isHit() && canBeHit()) {
-            float signAcc = passedTime - timePreempt;
-            if (Config.isFixFrameOffset()) {
-                signAcc += (float) hitOffsetToPreviousFrame() / 1000f;
-            }
+        float frameHitOffset = (float) hitOffsetToPreviousFrame() / 1000;
+
+        if (canBeHit(dt, frameHitOffset) && isHit()) {
+            // At this point, the object's state is already in the next update tick.
+            // However, hit judgements require the object's state to be in the previous tick.
+            // Therefore, we subtract dt to get the object's state in the previous tick.
+            float signAcc = passedTime - timePreempt - dt + frameHitOffset;
             final float acc = Math.abs(signAcc);
             if (acc <= GameHelper.getDifficultyHelper().hitWindowFor50(GameHelper.getOverallDifficulty())) {
                 playHitSamples();
@@ -321,8 +329,7 @@ public class GameplayHitCircle extends GameObject {
             listener.registerAccuracy(signAcc);
             passedTime = -1;
             // Remove circle and register hit in update thread
-            float finalSignAcc = signAcc;
-            listener.onCircleHit(id, finalSignAcc, position, endsCombo, (byte) 0, comboColor);
+            listener.onCircleHit(id, signAcc, position, endsCombo, (byte) 0, comboColor);
             removeFromScene();
         }
     }
