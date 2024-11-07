a1516336bec72ba60ec4d10bad02014979104665
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-11-07 17:36:02 +0800

    Submit full beatmap name without replacements to server upon playing

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import com.reco1l.osu.data.BeatmapInfo;
-import com.reco1l.toolkt.kotlin.StringsKt;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 
@@ -184,7 +183,7 @@ public class OnlineManager {
         PostBuilder post = new URLEncodedPostBuilder();
         post.addParam("userID", String.valueOf(userId));
         post.addParam("ssid", ssid);
-        post.addParam("filename", StringsKt.replaceAlphanumeric(beatmapInfo.getFullBeatmapName(), "_"));
+        post.addParam("filename", beatmapInfo.getFullBeatmapName());
         post.addParam("hash", hash);
         post.addParam("songTitle", beatmapInfo.getTitle());
         post.addParam("songArtist", beatmapInfo.getArtist());
