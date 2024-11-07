9329d9f7e4bfa262fe58245b47204f31b798a9de
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-11-07 08:18:06 +0800

    Fix wrong favorites filename in legacy database migration

--- src/com/reco1l/osu/data/Database.kt
+++ src/com/reco1l/osu/data/Database.kt
@@ -119,7 +119,7 @@ object DatabaseManager {
 
         // BeatmapCollections
         try {
-            val oldFavoritesFile = File(Config.getCorePath(), "json/favorites.json")
+            val oldFavoritesFile = File(Config.getCorePath(), "json/favorite.json")
 
             if (oldFavoritesFile.exists()) {
                 GlobalManager.getInstance().info = "Migrating beatmap collections..."
@@ -138,7 +138,7 @@ object DatabaseManager {
                     }
                 }
 
-                oldFavoritesFile.renameTo(File(Config.getCorePath(), "json/favorites_old.json"))
+                oldFavoritesFile.renameTo(File(Config.getCorePath(), "json/favorite_old.json"))
             }
 
         } catch (e: IOException) {
