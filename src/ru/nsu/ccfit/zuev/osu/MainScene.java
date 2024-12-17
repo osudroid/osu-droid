47c4ed264798f21b765908106f847c47b8b8b51c
Acivev <37246611+Acivev@users.noreply.github.com>
2024-12-17 12:51:04 +0100

    Make dev build text on top of everything.

--- src/ru/nsu/ccfit/zuev/osu/MainScene.java
+++ src/ru/nsu/ccfit/zuev/osu/MainScene.java
@@ -173,26 +173,6 @@ public class MainScene implements IUpdateHandler {
 
         menu = new MainMenu(this);
 
-        if (BuildConfig.DEBUG) {
-            ResourceManager.getInstance().loadHighQualityAsset("dev-build-overlay", "dev-build-overlay.png");
-
-            ExtendedSprite debugOverlay = new ExtendedSprite(ResourceManager.getInstance().getTexture("dev-build-overlay"));
-            debugOverlay.setPosition(Config.getRES_WIDTH() / 2f, Config.getRES_HEIGHT());
-            debugOverlay.setOrigin(Anchor.BottomCenter);
-            scene.attachChild(debugOverlay);
-
-            Text debugText = new Text(0, 0, ResourceManager.getInstance().getFont("smallFont"), "DEVELOPMENT BUILD");
-            debugText.setColor(1f, 237f / 255f, 0f);
-            debugText.setPosition((Config.getRES_WIDTH() - debugText.getWidth()) / 2f, Config.getRES_HEIGHT() - debugOverlay.getHeight() - 1f - debugText.getHeight());
-
-            Text debugTextShadow = new Text(0, 0, ResourceManager.getInstance().getFont("smallFont"), "DEVELOPMENT BUILD");
-            debugTextShadow.setColor(0f, 0f, 0f, 0.5f);
-            debugTextShadow.setPosition((Config.getRES_WIDTH() - debugText.getWidth()) / 2f + 2f, Config.getRES_HEIGHT() - debugOverlay.getHeight() - 1f - debugText.getHeight() + 2f);
-
-            scene.attachChild(debugTextShadow);
-            scene.attachChild(debugText);
-        }
-
         RoundedBox box = new RoundedBox() {
 
             {
@@ -475,6 +455,26 @@ public class MainScene implements IUpdateHandler {
 
         if (Config.isStayOnline()) {
             BannerLoader.loadBanner(scene);
+        }
+
+        if (BuildConfig.DEBUG) {
+            ResourceManager.getInstance().loadHighQualityAsset("dev-build-overlay", "dev-build-overlay.png");
+
+            ExtendedSprite debugOverlay = new ExtendedSprite(ResourceManager.getInstance().getTexture("dev-build-overlay"));
+            debugOverlay.setPosition(Config.getRES_WIDTH() / 2f, Config.getRES_HEIGHT());
+            debugOverlay.setOrigin(Anchor.BottomCenter);
+            scene.attachChild(debugOverlay);
+
+            Text debugText = new Text(0, 0, ResourceManager.getInstance().getFont("smallFont"), "DEVELOPMENT BUILD");
+            debugText.setColor(1f, 237f / 255f, 0f);
+            debugText.setPosition((Config.getRES_WIDTH() - debugText.getWidth()) / 2f, Config.getRES_HEIGHT() - debugOverlay.getHeight() - 1f - debugText.getHeight());
+
+            Text debugTextShadow = new Text(0, 0, ResourceManager.getInstance().getFont("smallFont"), "DEVELOPMENT BUILD");
+            debugTextShadow.setColor(0f, 0f, 0f, 0.5f);
+            debugTextShadow.setPosition((Config.getRES_WIDTH() - debugText.getWidth()) / 2f + 2f, Config.getRES_HEIGHT() - debugOverlay.getHeight() - 1f - debugText.getHeight() + 2f);
+
+            scene.attachChild(debugTextShadow);
+            scene.attachChild(debugText);
         }
 
         progressBar = new LinearSongProgress(scene, 0, 0, new PointF(Utils.toRes(Config.getRES_WIDTH() - 320), Utils.toRes(100)));
