10edb58ab04f4687e40a689e88a24059d357428e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-21 21:43:00 +0800

    Add `SliderTick` specific `timePreempt`

--- src/com/rian/osu/beatmap/hitobject/sliderobject/SliderTick.kt
+++ src/com/rian/osu/beatmap/hitobject/sliderobject/SliderTick.kt
@@ -1,6 +1,10 @@
 package com.rian.osu.beatmap.hitobject.sliderobject
 
+import com.rian.osu.GameMode
+import com.rian.osu.beatmap.sections.BeatmapControlPoints
+import com.rian.osu.beatmap.sections.BeatmapDifficulty
 import com.rian.osu.math.Vector2
+import kotlinx.coroutines.CoroutineScope
 
 /**
  * Represents a slider tick.
@@ -25,4 +29,19 @@ class SliderTick(
      * The start time of the span at which this [SliderTick] lies, in milliseconds.
      */
     private val spanStartTime: Double
-) : SliderHitObject(startTime, position)
+) : SliderHitObject(startTime, position) {
+    override fun applyDefaults(
+        controlPoints: BeatmapControlPoints,
+        difficulty: BeatmapDifficulty,
+        mode: GameMode,
+        scope: CoroutineScope?
+    ) {
+        super.applyDefaults(controlPoints, difficulty, mode, scope)
+
+        // Adding 200 to include the offset stable used.
+        // This is so on repeats ticks don't appear too late to be visually processed by the player.
+        val offset = if (spanIndex > 0) 200.0 else timePreempt * 0.66
+
+        timePreempt = (startTime - spanStartTime) / 2 + offset
+    }
+}
