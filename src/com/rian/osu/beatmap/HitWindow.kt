1bb40d6d0d6acec1a03e64d0236ba59f954cc2fb
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-30 14:32:44 +0800

    Add clearer description to `HitWindow` properties

--- src/com/rian/osu/beatmap/HitWindow.kt
+++ src/com/rian/osu/beatmap/HitWindow.kt
@@ -13,23 +13,23 @@ abstract class HitWindow @JvmOverloads constructor(
     constructor(overallDifficulty: Float?) : this(overallDifficulty ?: 5f)
 
     /**
-     * The hit window for 300 (Great) hit result.
+     * The hit window for 300 (Great) hit result in milliseconds.
      */
     abstract val greatWindow: Float
 
     /**
-     * The hit window for 100 (OK) hit result.
+     * The hit window for 100 (OK) hit result in milliseconds.
      */
     abstract val okWindow: Float
 
     /**
-     * The hit window for 50 (Meh) hit result.
+     * The hit window for 50 (Meh) hit result in milliseconds.
      */
     abstract val mehWindow: Float
 
     companion object {
         /**
-         * A fixed miss window regardless of difficulty settings.
+         * A fixed miss window regardless of difficulty settings in milliseconds.
          */
         const val MISS_WINDOW = 400f
     }
