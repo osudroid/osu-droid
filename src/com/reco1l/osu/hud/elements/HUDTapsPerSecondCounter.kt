683bb80d8a33be067b7e232c99fa1026e0aa4537
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-12 08:56:01 +0700

    Make `HUDTapsPerSecondCounter` inherit `HUDStatisticCounter`

--- src/com/reco1l/osu/hud/elements/HUDTapsPerSecondCounter.kt
+++ src/com/reco1l/osu/hud/elements/HUDTapsPerSecondCounter.kt
@@ -1,34 +1,15 @@
 package com.reco1l.osu.hud.elements
 
-import com.reco1l.andengine.text.ExtendedText
-import com.reco1l.osu.hud.HUDElement
-import com.reco1l.osu.playfield.SpriteFont
 import com.reco1l.osu.updateThread
-import ru.nsu.ccfit.zuev.osu.ResourceManager
 import ru.nsu.ccfit.zuev.osu.game.GameHelper
 import ru.nsu.ccfit.zuev.osu.game.GameScene
 import ru.nsu.ccfit.zuev.osu.scoring.StatisticV2
-import ru.nsu.ccfit.zuev.skins.OsuSkin
 
-class HUDTapsPerSecondCounter : HUDElement() {
+class HUDTapsPerSecondCounter : HUDStatisticCounter("Taps/sec") {
 
     override val name = "Taps per second counter"
 
-    private val label = ExtendedText().apply {
-        font = ResourceManager.getInstance().getFont("smallFont")
-        text = "Taps/sec"
-    }
-
-    private val value = SpriteFont(OsuSkin.get().scorePrefix).apply {
-        text = "0"
-    }
-
     private val timestamps = ArrayDeque<Float>(50)
-
-    init {
-        attachChild(label)
-        attachChild(value)
-    }
 
     override fun onGameplayTouchDown(time: Float) {
         // Queue to the update thread to avoid the timestamp queue potentially resizing itself in the main thread.
@@ -42,11 +23,6 @@ class HUDTapsPerSecondCounter : HUDElement() {
             timestamps.removeFirst()
         }
 
-        value.text = timestamps.size.toString()
-    }
-
-    override fun onManagedUpdate(pSecondsElapsed: Float) {
-        value.y = label.drawHeight
-        super.onManagedUpdate(pSecondsElapsed)
+        valueText.text = timestamps.size.toString()
     }
 }
