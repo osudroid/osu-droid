d043ffc277832162d502f3382862e0e58c492ed7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-14 17:39:16 +0700

    Update `ModTraceable` definition

--- src/com/rian/osu/mods/ModTraceable.kt
+++ src/com/rian/osu/mods/ModTraceable.kt
@@ -1,8 +1,17 @@
 package com.rian.osu.mods
 
+import ru.nsu.ccfit.zuev.osu.game.mods.GameMod
+
 /**
  * Represents the Traceable mod.
  */
-class ModTraceable : Mod() {
-    override val droidString = "b"
+class ModTraceable : Mod(), IModUserSelectable {
+    override val droidChar = 'b'
+    override val acronym = "TC"
+    override val enum = GameMod.MOD_TRACEABLE
+    override val textureNameSuffix = "traceable"
+
+    override fun equals(other: Any?) = other === this || other is ModTraceable
+    override fun hashCode() = super.hashCode()
+    override fun deepCopy() = ModTraceable()
 }
