5ca77c4d2caffe9e9a15fb14fced36607ca95af5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-22 04:51:22 +0700

    Remove unused import

--- src/ru/nsu/ccfit/zuev/osu/game/GameScene.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameScene.java
@@ -114,7 +114,6 @@ import ru.nsu.ccfit.zuev.osu.scoring.ScoringScene;
 import ru.nsu.ccfit.zuev.osu.scoring.StatisticV2;
 import ru.nsu.ccfit.zuev.osu.scoring.TouchType;
 import ru.nsu.ccfit.zuev.osuplus.BuildConfig;
-import ru.nsu.ccfit.zuev.osuplus.R;
 import ru.nsu.ccfit.zuev.skins.OsuSkin;
 import ru.nsu.ccfit.zuev.skins.BeatmapSkinManager;
 
