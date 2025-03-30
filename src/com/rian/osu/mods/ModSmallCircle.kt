ec278e0bef956ce5244d07835a864417a8a4d917
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-30 11:07:41 +0700

    Implement `IModApplicableToDifficulty` to `ModSmallCircle`
    Ensures that the mod is still applied properly in cases where it is not migrated.

--- src/com/rian/osu/mods/ModSmallCircle.kt
+++ src/com/rian/osu/mods/ModSmallCircle.kt
@@ -1,16 +1,27 @@
 package com.rian.osu.mods
 
+import com.rian.osu.GameMode
 import com.rian.osu.beatmap.sections.BeatmapDifficulty
+import com.rian.osu.utils.CircleSizeCalculator
 
 /**
  * Represents the Small Circle mod.
  */
-class ModSmallCircle : Mod(), IMigratableMod {
+class ModSmallCircle : Mod(), IModApplicableToDifficulty, IMigratableMod {
     override val name = "Small Circle"
     override val acronym = "SC"
     override val textureNameSuffix = "smallcircle"
 
     override fun migrate(difficulty: BeatmapDifficulty) = ModDifficultyAdjust(cs = difficulty.gameplayCS + 4)
+
+    override fun applyToDifficulty(mode: GameMode, difficulty: BeatmapDifficulty) {
+        difficulty.gameplayCS += 4
+
+        difficulty.difficultyCS = when (mode) {
+            GameMode.Droid -> CircleSizeCalculator.droidCSToDroidDifficultyScale(difficulty.gameplayCS)
+            GameMode.Standard -> difficulty.difficultyCS + 4
+        }
+    }
 
     override fun equals(other: Any?) = other === this || other is ModSmallCircle
     override fun hashCode() = super.hashCode()
