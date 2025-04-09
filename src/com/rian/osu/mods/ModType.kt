a911aa823bec84e7f025682982e21e0c0576193c
Reco1l <geronimoferruccio15@gmail.com>
2025-04-09 11:23:30 -0300

    Introduce localisation for mod type's names

--- src/com/rian/osu/mods/ModType.kt
+++ src/com/rian/osu/mods/ModType.kt
@@ -1,11 +1,19 @@
 package com.rian.osu.mods
 
+import androidx.annotation.*
+import ru.nsu.ccfit.zuev.osuplus.*
+
 /**
  * Available types of [Mod]s.
  */
-enum class ModType {
-    DifficultyReduction,
-    DifficultyIncrease,
-    Automation,
-    Conversion
+enum class ModType(
+    /**
+     * The string resource ID of the lcoalised mod type's name.
+     */
+    @StringRes val stringId: Int
+) {
+    DifficultyReduction(R.string.mod_section_difficulty_reduction),
+    DifficultyIncrease(R.string.mod_section_difficulty_increase),
+    Automation(R.string.mod_section_difficulty_automation),
+    Conversion(R.string.mod_section_difficulty_conversion)
 }
