b3fd25caf4210206a8d640dc1f4126b797275cf2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-04-12 00:11:01 +0800

    Add tests for `HitCircle`

---
+++ tests/test/kotlin/com/rian/osu/beatmap/hitobject/HitObjectTest.kt
@@ -0,0 +1,36 @@
+package com.rian.osu.beatmap.hitobject
+
+import com.rian.osu.GameMode
+import com.rian.osu.math.Vector2
+import org.junit.Assert
+
+sealed class HitObjectTest {
+    protected fun testStackedPositions(
+        obj: HitObject, mode: GameMode,
+        positionGetter: HitObject.() -> Vector2,
+        stackedPositionGetter: HitObject.() -> Vector2
+    ) = obj.apply {
+        difficultyScale = 1f
+
+        stackOffsetMultiplier = when (mode) {
+            GameMode.Droid -> 4f
+            GameMode.Standard -> -6.4f
+        }
+
+        fun testOffset() {
+            val positionOffset = stackedPositionGetter() - positionGetter()
+
+            Assert.assertEquals(positionOffset.x, difficultyStackOffset.x, 1e-2f)
+            Assert.assertEquals(positionOffset.y, difficultyStackOffset.y, 1e-2f)
+        }
+
+        difficultyStackHeight = 1
+        testOffset()
+
+        difficultyStackHeight = 2
+        testOffset()
+
+        difficultyStackHeight = 4
+        testOffset()
+    }
+}
