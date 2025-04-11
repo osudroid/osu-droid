c5fba680a099733f57dda587689f11e9c8f95761
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-04-11 20:00:52 +0800

    Add test for `LRUCache`

---
+++ tests/test/kotlin/com/rian/osu/utils/LRUCacheTest.kt
@@ -0,0 +1,34 @@
+package com.rian.osu.utils
+
+import org.junit.Assert
+import org.junit.Test
+
+class LRUCacheTest {
+    @Test
+    fun testLRUCache() {
+        val cache = LRUCache<Int, Int>(3)
+
+        cache.put(1, 1)
+        cache.put(2, 2)
+        cache.put(3, 3)
+
+        Assert.assertEquals(cache.get(1), 1)
+        Assert.assertEquals(cache.get(2), 2)
+        Assert.assertEquals(cache.get(3), 3)
+
+        cache.put(4, 4)
+
+        Assert.assertEquals(cache.get(1), null)
+        Assert.assertEquals(cache.get(2), 2)
+        Assert.assertEquals(cache.get(3), 3)
+        Assert.assertEquals(cache.get(4), 4)
+
+        cache.put(5, 5)
+
+        Assert.assertEquals(cache.get(1), null)
+        Assert.assertEquals(cache.get(2), null)
+        Assert.assertEquals(cache.get(3), 3)
+        Assert.assertEquals(cache.get(4), 4)
+        Assert.assertEquals(cache.get(5), 5)
+    }
+}
