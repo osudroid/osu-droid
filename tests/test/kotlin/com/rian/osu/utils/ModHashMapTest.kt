7eef171c28b3a3a9ef95ae1a4e3a5303339e3aae
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-04-12 16:21:22 +0800

    Add test for `ModHashMap.toLegacyModString`

--- tests/test/kotlin/com/rian/osu/utils/ModHashMapTest.kt
+++ tests/test/kotlin/com/rian/osu/utils/ModHashMapTest.kt
@@ -1,5 +1,6 @@
 package com.rian.osu.utils
 
+import com.rian.osu.mods.ModCustomSpeed
 import com.rian.osu.mods.ModDifficultyAdjust
 import com.rian.osu.mods.ModHardRock
 import com.rian.osu.mods.ModReallyEasy
@@ -34,5 +35,23 @@ class ModHashMapTest {
         map.put(ModDifficultyAdjust(1f, 1f, 1f, 1f))
 
         Assert.assertTrue(map.size == 1)
+    }
+
+    @Test
+    fun testLegacyModStringConversion() {
+        ModHashMap().apply {
+            put(ModReallyEasy())
+            put(ModHardRock())
+
+            Assert.assertEquals(toLegacyModString(), "rl|")
+
+            put(ModCustomSpeed(1.25f))
+
+            Assert.assertEquals(toLegacyModString(), "rl|x1.25")
+
+            put(ModDifficultyAdjust(cs = 4f, ar = 8f))
+
+            Assert.assertEquals(toLegacyModString(), "rl|x1.25|AR8.0|CS4.0")
+        }
     }
 }
