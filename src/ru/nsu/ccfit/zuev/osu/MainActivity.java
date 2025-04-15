f6f65dd9712a47bcf2146a6aaa6b4c65be0b9f5f
Reco1l <geronimoferruccio15@gmail.com>
2025-04-15 15:39:11 -0300

    Fix build error

--- src/ru/nsu/ccfit/zuev/osu/MainActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/MainActivity.java
@@ -42,7 +42,6 @@ import com.edlplan.ui.ActivityOverlay;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.reco1l.andengine.ExtendedEngine;
-import com.reco1l.andengine.TestScene;
 import com.reco1l.ibancho.LobbyAPI;
 import com.reco1l.osu.AccessibilityDetector;
 import com.reco1l.osu.DifficultyCalculationManager;
