c9802feaa86d49c83c09aeb090e69195fa214ee7
Reco1l <geronimoferruccio15@gmail.com>
2025-04-15 14:15:27 -0300

    Introduce EntityInspector

--- src/com/reco1l/BuildConfiguration.kt
+++ src/com/reco1l/BuildConfiguration.kt
@@ -13,6 +13,11 @@ object BuildConfiguration {
     const val SHOW_ENTITY_BOUNDARIES = false
 
     /**
+     * Whether to show the entity browser or not.
+     */
+    const val ENTITY_INSPECTOR = false
+
+    /**
      * Whether to use fake multiplayer mode or not.
      */
     const val MOCK_MULTIPLAYER = false
--- src/com/reco1l/andengine/ExtendedEntity.kt
+++ src/com/reco1l/andengine/ExtendedEntity.kt
@@ -545,7 +545,10 @@ abstract class ExtendedEntity : Entity(0f, 0f), ITouchArea, IModifierChain {
         foreground?.setSize(width, height)
         foreground?.onDraw(gl, camera)
 
-        if (BuildConfiguration.SHOW_ENTITY_BOUNDARIES) {
+
+        if ((BuildConfiguration.SHOW_ENTITY_BOUNDARIES || EntityInspector.SELECTED_ENTITY == this) && DEBUG_FOREGROUND != this) {
+            DEBUG_FOREGROUND.color = if (EntityInspector.SELECTED_ENTITY == this) ColorARGB(0xFF00FF00) else ColorARGB.White
+            DEBUG_FOREGROUND.lineWidth = if (EntityInspector.SELECTED_ENTITY == this) 3f else 1f
             DEBUG_FOREGROUND.setSize(width, height)
             DEBUG_FOREGROUND.onDraw(gl, camera)
         }
