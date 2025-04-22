45a6ec57e4ee0f3aa3c54e4293a1083d30b2c20e
Reco1l <geronimoferruccio15@gmail.com>
2025-04-22 10:13:28 -0300

    Introduce `scaleType` and `gravity` in `ExtendedSprite`

--- src/com/reco1l/andengine/sprite/ExtendedSprite.kt
+++ src/com/reco1l/andengine/sprite/ExtendedSprite.kt
@@ -4,14 +4,18 @@ import com.reco1l.andengine.*
 import com.reco1l.andengine.buffered.*
 import com.reco1l.andengine.info.*
 import com.reco1l.andengine.sprite.ExtendedSprite.*
+import com.reco1l.andengine.sprite.ScaleType.*
+import com.reco1l.framework.math.*
 import org.anddev.andengine.opengl.texture.region.*
 import org.anddev.andengine.opengl.util.*
 import javax.microedition.khronos.opengles.*
 import javax.microedition.khronos.opengles.GL11.*
+import kotlin.math.*
 
 /**
  * Sprite that allows to change texture once created.
  */
+@Suppress("LeakingThis")
 open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity<SpriteVBO>() {
 
     override var contentWidth: Float
@@ -26,7 +30,7 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
     /**
      * Whether the texture should be flipped horizontally.
      */
-    open var flippedHorizontal = false
+    var flippedHorizontal = false
         set(value) {
             field = value
             textureRegion?.isFlippedHorizontal = value
@@ -35,7 +39,7 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
     /**
      * Whether the texture should be flipped vertically.
      */
-    open var flippedVertical = false
+    var flippedVertical = false
         set(value) {
             field = value
             textureRegion?.isFlippedVertical = value
@@ -44,7 +48,7 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
     /**
      * The texture region of the sprite.
      */
-    open var textureRegion = textureRegion
+    var textureRegion = textureRegion
         set(value) {
             if (field != value) {
                 field = value
@@ -56,7 +60,7 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
     /**
      * The X position of the texture.
      */
-    open var textureX = 0
+    var textureX = 0
         set(value) {
             if (field != value) {
                 field = value
@@ -67,11 +71,35 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
     /**
      * The Y position of the texture.
      */
-    open var textureY = 0
+    var textureY = 0
         set(value) {
             if (field != value) {
                 field = value
                 textureRegion?.setTexturePosition(textureX, value)
+            }
+        }
+
+    /**
+     * The scale type of the sprite.
+     */
+    var scaleType: ScaleType = Fit
+        set(value) {
+            if (field != value) {
+                field = value
+                invalidateBuffer(BufferInvalidationFlag.Data)
+            }
+        }
+
+    /**
+     * The alignment of the texture.
+     *
+     * If the scale type is [ScaleType.Stretch] it will not take effect.
+     */
+    var gravity: Vec2 = Anchor.Center
+        set(value) {
+            if (field != value) {
+                field = value
+                invalidateBuffer(BufferInvalidationFlag.Data)
             }
         }
 
@@ -80,7 +108,6 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
         width = FitContent
         height = FitContent
 
-        @Suppress("LeakingThis")
         onTextureRegionChanged()
     }
 
@@ -120,8 +147,54 @@ open class ExtendedSprite(textureRegion: TextureRegion? = null) : BufferedEntity
         bufferUsage = GL_STATIC_DRAW
     ) {
         override fun update(gl: GL10, entity: BufferedEntity<*>, vararg data: Any) {
-            addQuad(0, 0f, 0f, width, height)
+
+            val textureWidth = contentWidth
+            val textureHeight = contentHeight
+
+            var quadWidth = textureWidth
+            var quadHeight = textureHeight
+
+            when (scaleType) {
+
+                Crop -> {
+                    val scale = max(width / textureWidth, height / textureHeight)
+                    quadWidth = textureWidth * scale
+                    quadHeight = textureHeight * scale
+                }
+
+                Fit -> {
+                    val scale = min(width / textureWidth, height / textureHeight)
+                    quadWidth = textureWidth * scale
+                    quadHeight = textureHeight * scale
+                }
+
+                Stretch -> Unit
+            }
+
+            val x = (width - quadWidth) * gravity.x
+            val y = (height - quadHeight) * gravity.y
+
+            addQuad(0, x, y, x + quadWidth, y + quadHeight)
         }
     }
 
+}
+
+enum class ScaleType {
+
+    /**
+     * Scale the texture to fill the entire sprite cropping the excess.
+     */
+    Crop,
+
+    /**
+     * Scale the texture to fit the sprite without cropping.
+     */
+    Fit,
+
+    /**
+     * Scale the texture to fit the sprite without cropping.
+     * The texture will be stretched to fill the entire sprite.
+     */
+    Stretch
 }
