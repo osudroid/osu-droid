9edc0d87ce773d6a226025101d44649425453f08
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-04-23 09:12:58 +0800

    Add serialization test for `ModTimeRamp`

---
+++ tests/test/kotlin/com/rian/osu/mods/ModTimeRampTest.kt
@@ -0,0 +1,32 @@
+package com.rian.osu.mods
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ModTimeRampTest {
+    @Test
+    fun `Test serialization`() {
+        TestModTimeRamp().serialize().getJSONObject("settings").apply {
+            Assert.assertEquals(1f, getDouble("initialRate").toFloat())
+            Assert.assertEquals(1.5f, getDouble("finalRate").toFloat())
+        }
+    }
+}
+
+private class TestModTimeRamp : ModTimeRamp() {
+    override val name = "Test"
+    override val acronym = "TS"
+    override val description = "Test mod"
+    override val type = ModType.Fun
+
+    override var initialRate = 1f
+    override var finalRate = 1.5f
+
+    override fun deepCopy() = TestModTimeRamp().also {
+        it.initialRate = initialRate
+        it.finalRate = finalRate
+    }
+}
