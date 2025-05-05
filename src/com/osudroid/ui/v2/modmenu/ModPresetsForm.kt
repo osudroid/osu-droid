ed4abba6aea80eb6449f2170d5de83e41981a149
Reco1l <geronimoferruccio15@gmail.com>
2025-05-05 14:29:41 -0300

    Revamp mod preset form modal

--- src/com/osudroid/ui/v2/modmenu/ModPresetsForm.kt
+++ src/com/osudroid/ui/v2/modmenu/ModPresetsForm.kt
@@ -1,10 +1,12 @@
 package com.osudroid.ui.v2.modmenu
 
+import com.osudroid.ui.v2.*
 import com.reco1l.andengine.*
 import com.reco1l.andengine.container.*
 import com.reco1l.andengine.ui.*
 import com.reco1l.andengine.ui.form.*
 import com.reco1l.framework.math.*
+import ru.nsu.ccfit.zuev.osu.*
 
 class ModPresetsForm : Modal(
 
@@ -20,18 +22,43 @@ class ModPresetsForm : Modal(
 ) {
 
     init {
+        staticBackdrop = true
+
         content.apply {
+
+            text {
+                width = FillParent
+                text = "New mod preset"
+                font = ResourceManager.getInstance().getFont("smallFont")
+                alignment = Anchor.Center
+                padding = Vec4(0f, 12f)
+            }
+
+            box {
+                width = FillParent
+                height = 1f
+                applyTheme = {
+                    color = it.accentColor
+                    alpha = 0.1f
+                }
+            }
 
             val nameInput = FormInput().apply {
                 label = "Name"
                 width = FillParent
+                showResetButton = false
             }
             +nameInput
+
+            +ModsIndicator(ModMenu.enabledMods).apply {
+                isExpanded = false
+                padding = Vec4(24f, 24f, 24f, 12f)
+            }
 
             linearContainer {
                 anchor = Anchor.TopRight
                 origin = Anchor.TopRight
-                padding = Vec4(12f)
+                padding = Vec4(24f)
                 spacing = 12f
 
                 textButton {
@@ -53,5 +80,4 @@ class ModPresetsForm : Modal(
         }
         show()
     }
-
 }
