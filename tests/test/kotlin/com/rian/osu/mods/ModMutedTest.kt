dc45148292ec83c2a4e387f2e03e0524e2d622d2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-11 11:23:52 +0800

    Fix `ModMuted.volumeAt` test

--- tests/test/kotlin/com/rian/osu/mods/ModMutedTest.kt
+++ tests/test/kotlin/com/rian/osu/mods/ModMutedTest.kt
@@ -39,19 +39,19 @@ class ModMutedTest {
             inverseMuting = false
             muteComboCount = 100
 
-            Assert.assertEquals(0f, volumeAt(-1), 1e-2f)
-            Assert.assertEquals(0f, volumeAt(0), 1e-2f)
+            Assert.assertEquals(1f, volumeAt(-1), 1e-2f)
+            Assert.assertEquals(1f, volumeAt(0), 1e-2f)
             Assert.assertEquals(0.25f, volumeAt(25), 1e-2f)
-            Assert.assertEquals(1f, volumeAt(100), 1e-2f)
-            Assert.assertEquals(1f, volumeAt(150), 1e-2f)
+            Assert.assertEquals(0f, volumeAt(100), 1e-2f)
+            Assert.assertEquals(0f, volumeAt(150), 1e-2f)
 
             inverseMuting = true
 
-            Assert.assertEquals(1f, volumeAt(-1), 1e-2f)
-            Assert.assertEquals(1f, volumeAt(0), 1e-2f)
+            Assert.assertEquals(0f, volumeAt(-1), 1e-2f)
+            Assert.assertEquals(0f, volumeAt(0), 1e-2f)
             Assert.assertEquals(0.5f, volumeAt(50), 1e-2f)
-            Assert.assertEquals(0f, volumeAt(100), 1e-2f)
-            Assert.assertEquals(0f, volumeAt(150), 1e-2f)
+            Assert.assertEquals(1f, volumeAt(100), 1e-2f)
+            Assert.assertEquals(1f, volumeAt(150), 1e-2f)
         }
     }
 
