195f6aa746528e33d08d5f6599dadd26774e42c0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-11 21:15:32 +0800

    Add `ModObjectScaleTween` as an incompatible mod to `ModTraceable`

--- src/com/rian/osu/mods/ModTraceable.kt
+++ src/com/rian/osu/mods/ModTraceable.kt
@@ -10,7 +10,7 @@ class ModTraceable : Mod() {
     override val acronym = "TC"
     override val description = "Put your faith in the approach circles..."
     override val type = ModType.DifficultyIncrease
-    override val incompatibleMods = super.incompatibleMods + ModHidden::class
+    override val incompatibleMods = super.incompatibleMods + arrayOf(ModHidden::class, ModObjectScaleTween::class)
 
     override fun calculateScoreMultiplier(difficulty: BeatmapDifficulty) = 1.06f
     override fun deepCopy() = ModTraceable()
