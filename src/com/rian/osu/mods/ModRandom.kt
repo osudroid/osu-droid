b07b2ab3553ac899f7eca421b370f9970913d930
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-14 19:52:21 +0800

    Use `NullableIntegerModSetting` for `ModRandom.seed`

--- src/com/rian/osu/mods/ModRandom.kt
+++ src/com/rian/osu/mods/ModRandom.kt
@@ -22,8 +22,14 @@ class ModRandom : Mod(), IModApplicableToBeatmap {
     override val description = "It never gets boring!"
     override val type = ModType.Conversion
 
-    // TODO: Make this a mod setting once user input is possible
-    var seed: Int? = null
+    var seed by NullableIntegerModSetting(
+        name = "Seed",
+        valueFormatter = { it.toString() },
+        defaultValue = null,
+        minValue = 0,
+        orderPosition = 0,
+        useManualInput = true
+    )
 
     /**
      * Defines how sharp the angles of [HitObject]s should be.
