ed998e692e64f07901c051717fdc620137860eaa
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-14 19:31:59 +0800

    Add `NullableIntegerModSetting`

--- src/com/rian/osu/mods/ModSetting.kt
+++ src/com/rian/osu/mods/ModSetting.kt
@@ -225,6 +225,89 @@ open class IntegerModSetting(
     }
 }
 
+open class NullableIntegerModSetting(
+    name: String,
+    valueFormatter: (Int?) -> String = { it.toString() },
+    defaultValue: Int?,
+    minValue: Int = Int.MIN_VALUE,
+    maxValue: Int = Int.MAX_VALUE,
+    step: Int = 1,
+    orderPosition: Int? = null,
+
+    /**
+     * Whether to allow the user to input the value of this [NullableIntegerModSetting] manually.
+     */
+    val useManualInput: Boolean = false
+
+) : RangeConstrainedModSetting<Int?>(name, valueFormatter, defaultValue, minValue, maxValue, step, orderPosition) {
+    override var defaultValue
+        get() = super.defaultValue
+        set(value) {
+            if (value != null && value !in minValue!!..maxValue!!) {
+                throw IllegalArgumentException("defaultValue must be between minValue and maxValue.")
+            }
+
+            super.defaultValue = value
+        }
+
+    override var minValue
+        get() = super.minValue
+        set(value) {
+            if (value == null) {
+                throw IllegalArgumentException("minValue cannot be null.")
+            }
+
+            if (value > maxValue!!) {
+                throw IllegalArgumentException("minValue cannot be greater than maxValue.")
+            }
+
+            super.minValue = value
+        }
+
+    override var maxValue
+        get() = super.maxValue
+        set(value) {
+            if (value == null) {
+                throw IllegalArgumentException("maxValue cannot be null.")
+            }
+
+            if (value < minValue!!) {
+                throw IllegalArgumentException("maxValue cannot be less than minValue.")
+            }
+
+            super.maxValue = value
+        }
+
+    override var step
+        get() = super.step
+        set(value) {
+            if (value == null) {
+                throw IllegalArgumentException("step cannot be null.")
+            }
+
+            if (value < 0) {
+                throw IllegalArgumentException("step cannot be less than 0.")
+            }
+
+            super.step = value
+        }
+
+    init {
+        require(minValue <= maxValue) { "minValue must be less than or equal to maxValue." }
+        require(step >= 0f) { "step must be greater than or equal to 0." }
+        require(defaultValue == null || defaultValue in minValue..maxValue) { "defaultValue must be between minValue and maxValue." }
+    }
+
+    override fun processValue(value: Int?) = when {
+        value == null -> null
+        value < minValue!! -> minValue
+        value > maxValue!! -> maxValue
+        step == 0 -> value
+
+        else -> (round((value - minValue!!) / step!!.toFloat()) * step!! + minValue!!).roundToInt()
+    }
+}
+
 open class FloatModSetting(
     name: String,
     valueFormatter: (Float) -> String = { it.toString() },
