5e3e9697c7faf0505fcdb7b978cdee8a4ced5796
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-18 12:37:20 +0800

    Add KDoc to `ModSettingComponent`

--- src/com/osudroid/ui/v2/modmenu/ModSettingComponent.kt
+++ src/com/osudroid/ui/v2/modmenu/ModSettingComponent.kt
@@ -6,16 +6,34 @@ import com.reco1l.andengine.ui.form.FormControl
 import com.rian.osu.mods.Mod
 import com.rian.osu.mods.ModSetting
 
+/**
+ * Interface for a component that controls a [ModSetting].
+ */
 interface IModSettingComponent<V : Any?> {
+    /**
+     * The [ModSetting] that is controlled by this [IModSettingComponent].
+     */
     val setting: ModSetting<V>
+
+    /**
+     * Updates this [IModSettingComponent] to reflect the current state of the [ModSetting].
+     */
     fun update()
 }
 
+/**
+ * A component that controls a [ModSetting].
+ *
+ * @param TSettingValue The type of the value in the [ModSetting].
+ * @param TControlValue The type of the value in the [FormControl] that is used to display this [ModSettingComponent].
+ */
 sealed class ModSettingComponent<TSettingValue : Any?, TControlValue : Any>(
     val mod: Mod,
     override val setting: ModSetting<TSettingValue>
 ) : Container(), IModSettingComponent<TSettingValue> {
-
+    /**
+     * The [FormControl] that is used to display this [ModSettingComponent]
+     */
     protected val control = createControl().apply {
         width = FillParent
     }
@@ -41,7 +59,18 @@ sealed class ModSettingComponent<TSettingValue : Any?, TControlValue : Any>(
         }
     }
 
+    /**
+     * Creates the [FormControl] that is used to display this [ModSettingComponent].
+     */
     protected abstract fun createControl(): FormControl<TControlValue, Control<TControlValue>>
+
+    /**
+     * Converts a value from the [ModSetting] to a value that can be displayed in the [FormControl].
+     */
     protected abstract fun convertSettingValue(value: TSettingValue): TControlValue
+
+    /**
+     * Converts a value from the [FormControl] to a value that can be used in the [ModSetting].
+     */
     protected abstract fun convertControlValue(value: TControlValue): TSettingValue?
 }
