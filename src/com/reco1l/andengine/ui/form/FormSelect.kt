ad00ed096a2c9841966d4c7fcc29d7f0ae56b708
Reco1l <geronimoferruccio15@gmail.com>
2025-05-28 14:48:06 -0300

    Introduce Select and FormSelect control

---
+++ src/com/reco1l/andengine/ui/form/FormSelect.kt
@@ -0,0 +1,40 @@
+package com.reco1l.andengine.ui.form
+
+import com.reco1l.andengine.*
+import com.reco1l.andengine.container.*
+import com.reco1l.andengine.ui.*
+import com.reco1l.framework.math.*
+
+@Suppress("LeakingThis")
+open class FormSelect<T : Any>(initialValues: List<T> = emptyList()) : FormControl<List<T>, Select<T>>(initialValues) {
+
+    final override val control = Select<T>().apply {
+        width = FillParent
+    }
+
+    // Value is already reflected in the control.
+    override val valueText = null
+
+
+    var selectionMode by control::selectionMode
+
+    var placeholder by control::placeholder
+
+    var options by control::options
+
+
+    init {
+        orientation = Orientation.Vertical
+        spacing = 12f
+
+        linearContainer {
+            width = FillParent
+            padding = Vec4(0f, 12f)
+            spacing = 12f
+            +labelText
+            +resetButton
+        }
+        +control
+    }
+
+}
