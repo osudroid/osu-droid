220ea2e7552c546b0d7a0c28abefd0404500f7d4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-31 16:08:40 +0800

    Update `ModDifficultyAdjust` tests

--- tests/test/kotlin/com/rian/osu/mods/ModDifficultyAdjustTest.kt
+++ tests/test/kotlin/com/rian/osu/mods/ModDifficultyAdjustTest.kt
@@ -50,6 +50,15 @@ class ModDifficultyAdjustTest {
         BeatmapDifficulty().apply {
             ModDifficultyAdjust(ar = 9f).applyToDifficulty(GameMode.Droid, this, listOf(ModDoubleTime()))
 
+            Assert.assertEquals(9f, ar)
+        }
+    }
+
+    @Test
+    fun `Test AR override with non-1x speed multiplier with old scaling`() {
+        BeatmapDifficulty().apply {
+            ModDifficultyAdjust(ar = 9f).applyToDifficulty(GameMode.Droid, this, listOf(ModDoubleTime(), ModReplayV6()))
+
             Assert.assertEquals(7f, ar)
         }
     }
@@ -57,6 +66,37 @@ class ModDifficultyAdjustTest {
     @Test
     fun `Test object fade in adjustments with non-1x speed multiplier AR override`() {
         val mods = listOf(ModDoubleTime())
+        val difficulty = BeatmapDifficulty()
+        val difficultyAdjust = ModDifficultyAdjust(ar = 9f)
+
+        difficultyAdjust.applyToDifficulty(GameMode.Droid, difficulty, mods)
+
+        Slider(
+            0.0, Vector2(0), 0, SliderPath(
+                SliderPathType.Linear, listOf(Vector2(0), Vector2(256, 0)), 256.0
+            ), true, 0, mutableListOf()
+        ).apply {
+            applyDefaults(BeatmapControlPoints(), difficulty, GameMode.Droid)
+            difficultyAdjust.applyToHitObject(GameMode.Droid, this, mods)
+
+            Assert.assertEquals(timePreempt, 600.0, 1e-2)
+            Assert.assertEquals(timeFadeIn, 400.0, 1e-2)
+
+            Assert.assertEquals(head.timePreempt, 600.0, 1e-2)
+            Assert.assertEquals(head.timeFadeIn, 400.0, 1e-2)
+
+            Assert.assertTrue(nestedHitObjects.size > 2)
+
+            val tick = nestedHitObjects[1]
+
+            Assert.assertEquals(tick.timePreempt, 896.0, 1e-2)
+            Assert.assertEquals(tick.timeFadeIn, 400.0, 1e-2)
+        }
+    }
+
+    @Test
+    fun `Test object fade in adjustments with non-1x speed multiplier AR override with old scaling`() {
+        val mods = listOf(ModDoubleTime(), ModReplayV6())
         val difficulty = BeatmapDifficulty()
         val difficultyAdjust = ModDifficultyAdjust(ar = 9f)
 
