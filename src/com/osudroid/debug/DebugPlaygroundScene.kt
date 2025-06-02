80ba334dbce144ad1d581ad41ee9808fa05b0e18
Reco1l <geronimoferruccio15@gmail.com>
2025-06-02 12:18:16 -0300

    Fix build error

--- src/com/osudroid/debug/DebugPlaygroundScene.kt
+++ src/com/osudroid/debug/DebugPlaygroundScene.kt
@@ -2,7 +2,7 @@ package com.osudroid.debug
 
 import com.reco1l.andengine.*
 
-object DebugPlaygroundScene : ExtendedScene() {
+object DebugPlaygroundScene : UIScene() {
     init {
         // Test here...
     }
