1f885cb7199724ae0d9b22948dc03dfcfb00b826
Reco1l <geronimoferruccio15@gmail.com>
2025-06-05 13:59:09 -0300

    Implement flex container tests

---
+++ tests/test/kotlin/com/reco1l/andengine/UIFlexContainerTest.kt
@@ -0,0 +1,45 @@
+package com.reco1l.andengine
+
+import com.reco1l.andengine.container.*
+import junit.framework.TestCase.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class UIFlexContainerTest {
+
+    @Test
+    fun `Test flex container flex-grow`() {
+
+        val component1: UIDummyComponent
+        val component2: UIDummyComponent
+
+        UIFlexContainer().apply {
+            width = 100f
+
+            +UIDummyComponent().apply {
+                height = 100f
+                flexRules {
+                    grow = 2f
+                }
+                component1 = this
+            }
+
+            +UIDummyComponent().apply {
+                width = 100f
+
+                flexRules {
+                    grow = 1f
+                }
+                component2 = this
+            }
+
+            onHandleInvalidations()
+        }
+
+        assertEquals(component1.width, 100f * (2f / 3f))
+        assertEquals(component2.width, 100f * (1f / 3f))
+    }
+
+}
