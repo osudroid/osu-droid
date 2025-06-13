3611b37aa760e7ee9e2aa9565388f4dd2845fa69
Reco1l <geronimoferruccio15@gmail.com>
2025-06-13 14:27:01 -0300

    Disable textures on `UISprite` if there's no texture region set

--- src/com/reco1l/andengine/sprite/UISprite.kt
+++ src/com/reco1l/andengine/sprite/UISprite.kt
@@ -141,6 +141,13 @@ open class UISprite(textureRegion: TextureRegion? = null) : UIBufferedComponent<
 
     override fun beginDraw(gl: GL10) {
         super.beginDraw(gl)
+
+        if (textureRegion == null) {
+            GLHelper.disableTextures(gl)
+            GLHelper.disableTexCoordArray(gl)
+            return
+        }
+
         GLHelper.enableTextures(gl)
         GLHelper.enableTexCoordArray(gl)
     }
