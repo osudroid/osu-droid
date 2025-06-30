34310a4653d4c9d4c3304c0acd9903e8de0e8cc3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-06-30 18:51:55 +0800

    Add first adjustable object for `ModHidden` and `ModTraceable`

--- src/com/rian/osu/mods/ModTraceable.kt
+++ src/com/rian/osu/mods/ModTraceable.kt
@@ -1,17 +1,20 @@
 package com.rian.osu.mods
 
+import com.rian.osu.beatmap.hitobject.HitObject
+import com.rian.osu.beatmap.hitobject.Spinner
 import com.rian.osu.beatmap.sections.BeatmapDifficulty
 
 /**
  * Represents the Traceable mod.
  */
-class ModTraceable : Mod() {
+class ModTraceable : ModWithVisibilityAdjustment() {
     override val name = "Traceable"
     override val acronym = "TC"
     override val description = "Put your faith in the approach circles..."
     override val type = ModType.DifficultyIncrease
     override val incompatibleMods = super.incompatibleMods + ModHidden::class
 
+    override fun isFirstAdjustableObject(hitObject: HitObject) = hitObject !is Spinner
     override fun calculateScoreMultiplier(difficulty: BeatmapDifficulty) = 1.06f
     override fun deepCopy() = ModTraceable()
 }
