d2c2e66097d271c3a9fff683e02ea2b6b48776b0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-07-04 09:16:00 +0800

    Add comment explaining NaN check in `HUDPPCounter`

--- src/com/osudroid/ui/v2/hud/elements/HUDPPCounter.kt
+++ src/com/osudroid/ui/v2/hud/elements/HUDPPCounter.kt
@@ -21,6 +21,9 @@ class HUDPPCounter : HUDElement() {
     }
 
     fun setValue(value: Double) {
+        // The NaN check here is necessary as calculations may result in NaN values, which should not be displayed.
+        // This is not a foolproof solution, but is enough to prevent crashes from happening.
+        // Until the source of the NaN values is found and fixed, this must remain in place.
         sprite.text = "${(value.takeIf { !it.isNaN() } ?: 0.0).roundToInt()}${if (Config.getDifficultyAlgorithm() == DifficultyAlgorithm.droid) "dpp" else "pp"}"
     }
 
