efdb411eb3f22619357c148090f1b93d78609df5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-07-19 23:42:34 +0800

    Prioritize native `String.toFloat` over comma separator in `String.toFloatWithCommaSeparator`

--- src/com/rian/util/Strings.kt
+++ src/com/rian/util/Strings.kt
@@ -5,20 +5,19 @@ import java.text.DecimalFormatSymbols
 import java.util.Locale
 
 /**
- * Attempts to parse a [String] to a [Float] where the [String] uses a comma as the decimal separator.
+ * Attempts to parse a [String] to a [Float] where the [String] may use a comma as the decimal separator.
  *
- * If the parsing fails, this method fallbacks to the original [String.toFloat] method.
+ * First, the method will try to use [String.toFloat]. If that fails, it will assume the [String]
+ * uses a comma as the decimal separator and will attempt to parse it as such.
  *
  * @return The parsed [Float] value.
  */
-fun String.toFloatWithCommaSeparator(): Float {
+fun String.toFloatWithCommaSeparator() = try {
+    toFloat()
+} catch (_: Exception) {
     // Locale.GERMAN uses comma as decimal separator, so we use it for sample
     val symbols = DecimalFormatSymbols(Locale.GERMAN)
     val format = DecimalFormat("#,##0.00", symbols)
 
-    return try {
-        format.parse(this)!!.toFloat()
-    } catch (_: Exception) {
-        toFloat()
-    }
+    format.parse(this)!!.toFloat()
 }
