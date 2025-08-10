764b405c21d99cd2952e9e54976d590be957cb17
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-08-10 15:55:10 +0700

    Reduce screen space playfield size by 5%
    This makes `MAP_ACTUAL_HEIGHT` inline with how `MAP_HEIGHT` is obtained, that is from `480 * 0.8` (which is `384`).
    
    This change will make circles smaller, but by such a small fraction that it would not matter to most players.

--- src/ru/nsu/ccfit/zuev/osu/Constants.java
+++ src/ru/nsu/ccfit/zuev/osu/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final int MAP_HEIGHT = 384;
     public static final int MAP_ACTUAL_WIDTH_OLD = 820;
     public static final int MAP_ACTUAL_HEIGHT_OLD = 570;
-    public static final int MAP_ACTUAL_HEIGHT = (int) (Config.getRES_HEIGHT() * 0.85f);
+    public static final int MAP_ACTUAL_HEIGHT = (int) (Config.getRES_HEIGHT() * 0.8f);
     public static final int MAP_ACTUAL_WIDTH = MAP_ACTUAL_HEIGHT / 3 * 4;
 
     private Constants() {
