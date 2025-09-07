d1be5766b9bac003e51cee75400aa8b71e56ac77
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-09-07 08:54:53 +0700

    Update difficulty calculation test

--- tests/test/kotlin/com/rian/osu/difficulty/calculator/DroidDifficultyCalculatorTest.kt
+++ tests/test/kotlin/com/rian/osu/difficulty/calculator/DroidDifficultyCalculatorTest.kt
@@ -36,12 +36,12 @@ class DroidDifficultyCalculatorTest {
 
         calculator.calculate(beatmap).apply {
             // These results are off by a margin from server-side results due to floating point differences.
-            Assert.assertEquals(2.453388340939304, aimDifficulty, 1e-5)
-            Assert.assertEquals(1.4928164438079188, tapDifficulty, 1e-5)
-            Assert.assertEquals(0.8031331688998974, rhythmDifficulty, 1e-5)
+            Assert.assertEquals(2.495586735329097, aimDifficulty, 1e-5)
+            Assert.assertEquals(1.4808324669581225, tapDifficulty, 1e-5)
+            Assert.assertEquals(0.812969288725491, rhythmDifficulty, 1e-5)
             Assert.assertEquals(0.0, flashlightDifficulty, 1e-5)
-            Assert.assertEquals(0.8056919851984994, visualDifficulty, 1e-5)
-            Assert.assertEquals(3.973919453420672, starRating, 1e-6)
+            Assert.assertEquals(0.8058573101651171, visualDifficulty, 1e-5)
+            Assert.assertEquals(3.9986518471813386, starRating, 1e-6)
         }
     }
 }
