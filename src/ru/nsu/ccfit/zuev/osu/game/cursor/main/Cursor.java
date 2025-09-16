06a3a95a230c8ddd8c8c0e9a9ceb26418dfdb326
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-09-16 22:59:40 +0700

    Rework `Cursor` to be able to store multiple events at once

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/main/Cursor.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/main/Cursor.java
@@ -1,13 +1,57 @@
 package ru.nsu.ccfit.zuev.osu.game.cursor.main;
 
-import android.graphics.PointF;
+import androidx.annotation.Nullable;
 
+import com.osudroid.game.CursorEvent;
+
+import org.anddev.andengine.input.touch.TouchEvent;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a cursor in gameplay.
+ */
 public class Cursor {
-    public PointF mousePos = new PointF(0, 0);
-    public PointF trackPos = new PointF(0, 0);
+    /**
+     * Whether this {@link Cursor} is blocked from a {@link TouchEvent#ACTION_DOWN}.
+     */
     public boolean mouseBlocked = false;
+
+    /**
+     * Whether the {@link Cursor} is currently pressed down.
+     */
     public boolean mouseDown = false;
-    public boolean mouseOldDown = false;
-    public boolean mousePressed = false;
-    public double mouseDownOffsetMS = 0;
+
+    /**
+     * A list of {@link CursorEvent}s of this {@link Cursor} that has not been processed yet.
+     */
+    public final ArrayList<CursorEvent> events = new ArrayList<>(25);
+
+    /**
+     * Obtains the latest {@link CursorEvent} of this {@link Cursor}.
+     *
+     * @param actions The actions to filter by. If none are provided, no filtering is done.
+     * @return The latest {@link CursorEvent}, or {@code null} if there are no {@link CursorEvent}s.
+     */
+    @Nullable
+    public CursorEvent getLatestEvent(int ...actions) {
+        if (actions.length == 0) {
+            return events.isEmpty() ? null : events.get(events.size() - 1);
+        }
+
+        int size = events.size();
+
+        for (int i = size - 1; i >= 0; i--) {
+            var event = events.get(i);
+
+            //noinspection ForLoopReplaceableByForEach
+            for (int j = 0; j < actions.length; j++) {
+                if (event.action == actions[j]) {
+                    return event;
+                }
+            }
+        }
+
+        return null;
+    }
 }
