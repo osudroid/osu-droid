796e7737e3985d904f008d7772b6497b5259cbee
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-09-16 22:59:10 +0700

    Add class for processing `TouchEvent`s in gameplay
    Since `TouchEvent` gets pooled by the engine, we cannot store it in a class, so we use our own state class.

---
+++ src/com/osudroid/game/CursorEvent.kt
@@ -0,0 +1,117 @@
+package com.osudroid.game
+
+import android.graphics.PointF
+import androidx.core.util.Pools.SynchronizedPool
+import org.anddev.andengine.input.touch.TouchEvent
+import ru.nsu.ccfit.zuev.osu.Config
+import ru.nsu.ccfit.zuev.osu.Constants
+import ru.nsu.ccfit.zuev.osu.game.GameHelper
+
+/**
+ * Represents a cursor event in gameplay.
+ */
+class CursorEvent {
+    private constructor()
+
+    /**
+     * The time at which the [CursorEvent] occurred, in the [System.currentTimeMillis] time base.
+     */
+    @JvmField
+    var time = 0L
+
+    /**
+     * The action of the [CursorEvent], which can be one of [TouchEvent.ACTION_DOWN], [TouchEvent.ACTION_MOVE], or [TouchEvent.ACTION_UP].
+     */
+    @JvmField
+    var action = TouchEvent.ACTION_DOWN
+
+    /**
+     * Whether the action is [TouchEvent.ACTION_DOWN].
+     */
+    val isActionDown
+        get() = action == TouchEvent.ACTION_DOWN
+
+    /**
+     * Whether the action is [TouchEvent.ACTION_MOVE].
+     */
+    val isActionMove
+        get() = action == TouchEvent.ACTION_MOVE
+
+    /**
+     * Whether the action is [TouchEvent.ACTION_UP].
+     */
+    val isActionUp
+        get() = action == TouchEvent.ACTION_UP
+
+    /**
+     * The offset of the [CursorEvent] from the last frame, in milliseconds.
+     */
+    @JvmField
+    var offset = 0.0
+
+    /**
+     * The position of the cursor in screen space.
+     */
+    @JvmField
+    var position = PointF()
+
+    /**
+     * The position of the cursor in playfield space.
+     */
+    @JvmField
+    var trackPosition = PointF()
+
+    /**
+     * Releases this [CursorEvent] back to the pool for reuse.
+     */
+    fun release() {
+        pool.release(this)
+    }
+
+    private fun apply(event: TouchEvent) {
+        time = event.motionEvent.eventTime
+        action = event.action
+        offset = 0.0
+
+        val width = Config.getRES_WIDTH().toFloat()
+        val height = Config.getRES_HEIGHT().toFloat()
+
+        position.x = event.x.coerceIn(0f, width)
+        position.y = event.y.coerceIn(0f, height)
+
+        trackPosition.x = event.x
+        trackPosition.y = event.y
+
+        if (GameHelper.isHardRock()) {
+            trackPosition.y -= height / 2f
+            trackPosition.y *= -1
+            trackPosition.y += height / 2f
+        }
+
+        trackPosition.x -= (width - Constants.MAP_ACTUAL_WIDTH) / 2f
+        trackPosition.y -= (height - Constants.MAP_ACTUAL_HEIGHT) / 2f
+
+        trackPosition.x *= Constants.MAP_WIDTH / Constants.MAP_ACTUAL_WIDTH.toFloat()
+        trackPosition.y *= Constants.MAP_HEIGHT / Constants.MAP_ACTUAL_HEIGHT.toFloat()
+    }
+
+    companion object {
+        private val pool = SynchronizedPool<CursorEvent>(50)
+
+        /**
+         * Obtains an instance of [CursorEvent] from the pool or creates a new one if the pool is empty.
+         *
+         * @return An instance of [CursorEvent].
+         */
+        @JvmStatic
+        fun obtain() = pool.acquire() ?: CursorEvent()
+
+        /**
+         * Obtains an instance of [CursorEvent] from the pool or creates a new one if the pool is empty.
+         *
+         * @return An instance of [CursorEvent].
+         */
+        @JvmStatic
+        fun obtain(event: TouchEvent) = obtain().also { it.apply(event) }
+    }
+}
