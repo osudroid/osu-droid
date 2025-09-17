3f49a158d508be99a0f98fe9bd90a0a7cbe874d1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-09-17 19:37:51 +0700

    Add method to obtain hitting cursor of a `HitObject`

--- src/ru/nsu/ccfit/zuev/osu/game/GameObject.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameObject.java
@@ -2,8 +2,13 @@ package ru.nsu.ccfit.zuev.osu.game;
 
 import android.graphics.PointF;
 
-import com.rian.osu.beatmap.HitWindow;
+import androidx.annotation.Nullable;
 
+import com.osudroid.game.CursorEvent;
+import com.rian.osu.beatmap.HitWindow;
+import com.rian.osu.beatmap.hitobject.HitObject;
+
+import ru.nsu.ccfit.zuev.osu.Utils;
 import ru.nsu.ccfit.zuev.osu.scoring.Replay;
 
 public abstract class GameObject {
@@ -58,4 +63,140 @@ public abstract class GameObject {
     }
 
     public void stopLoopingSamples() {}
+
+    /**
+     * Obtains the {@link CursorEvent} that hits this {@link GameObject}, if any.
+     *
+     * @param listener The {@link GameObjectListener} to get cursors from.
+     * @param hitObject The {@link HitObject} associated with this {@link GameObject}.
+     * @param objectElapsedTime The elapsed time since the start of this {@link GameObject}, in seconds.
+     * @return The {@link CursorEvent} that hits this {@link GameObject}, or {@code null} if none.
+     */
+    @Nullable
+    protected final CursorEvent getHittingCursor(GameObjectListener listener, HitObject hitObject,
+                                                 double objectElapsedTime) {
+        int cursorCount = listener.getCursorsCount();
+
+        // For Autopilot, we only need to check if a cursor is pressed.
+        if (GameHelper.isAutopilot()) {
+            for (int i = 0; i < cursorCount; i++) {
+                var cursor = listener.getCursor(i);
+                var events = cursor.downEvents;
+                int size = events.size();
+
+                while (cursor.latestProcessedDownEventIndex < size) {
+                    var event = events.get(cursor.latestProcessedDownEventIndex++);
+
+                    if (canHit(event)) {
+                        return event;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        // For Relax, we need to check two cases:
+        // 1. If a cursor flows over the object when it is hittable.
+        // 2. If a cursor is pressed while it is over the object when it is hittable.
+        if (GameHelper.isRelax()) {
+            for (int i = 0; i < cursorCount; i++) {
+                var cursor = listener.getCursor(i);
+                var events = cursor.events;
+                int size = events.size();
+
+                while (cursor.latestProcessedEventIndex < size) {
+                    var event = events.get(cursor.latestProcessedEventIndex++);
+
+                    if (event.isActionUp()) {
+                        continue;
+                    }
+
+                    boolean isHit = isHit(hitObject, event);
+
+                    // Case 1
+                    if (event.isActionDown() && isHit && canHit(event)) {
+                        return event;
+                    }
+
+                    // Case 2
+                    if (objectElapsedTime >= 0 && isHit) {
+                        return event;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        // In regular gameplay, we need to iteratively check whether one or more cursors is on the object while a cursor
+        // is being pressed.
+        for (int i = 0; i < cursorCount; i++) {
+            var cursor = listener.getCursor(i);
+            var downEvents = cursor.downEvents;
+            int downEventsSize = downEvents.size();
+
+            while (cursor.latestProcessedDownEventIndex < downEventsSize) {
+                var downEvent = downEvents.get(cursor.latestProcessedDownEventIndex++);
+
+                if (!canHit(downEvent)) {
+                    continue;
+                }
+
+                for (int j = 0; j < cursorCount; j++) {
+                    if (i == j) {
+                        // For the cursor that generated the down event, check if the event position is on the object.
+                        if (isHit(hitObject, downEvent)) {
+                            return downEvent;
+                        }
+                    } else {
+                        // For the other case, we need to check the event closest to the down event and check if it is
+                        // on the object.
+                        var otherCursor = listener.getCursor(j);
+                        var closestEvent = otherCursor.getClosestEventBefore(downEvent.systemTime);
+
+                        if (closestEvent == null || closestEvent.isActionUp()) {
+                            continue;
+                        }
+
+                        if (isHit(hitObject, closestEvent)) {
+                            return downEvent;
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Determines whether a {@link CursorEvent} can hit this {@link GameObject}.
+     *
+     * @param cursorEvent The {@link CursorEvent} to check.
+     * @return {@code true} if the {@link CursorEvent} can hit this {@link GameObject}, {@code false} otherwise.
+     */
+    private boolean canHit(CursorEvent cursorEvent) {
+        if (cursorEvent.isActionUp()) {
+            return false;
+        }
+
+        return (cursorEvent.trackTime + cursorEvent.offset) / 1000 >= hitTime - objectHittableRange;
+    }
+
+    /**
+     * Determines whether a {@link CursorEvent} hits this {@link GameObject}. The determination is solely based on
+     * distance, not considering timing.
+     *
+     * @param hitObject The {@link HitObject} associated with this {@link GameObject}.
+     * @param cursorEvent The {@link CursorEvent} to check.
+     * @return {@code true} if the {@link CursorEvent} hits this {@link GameObject}, {@code false} otherwise.
+     */
+    private boolean isHit(HitObject hitObject, CursorEvent cursorEvent) {
+        if (cursorEvent.isActionUp()) {
+            return false;
+        }
+
+        return Utils.squaredDistance(position, cursorEvent.position) <= Utils.sqr((float) hitObject.getScreenSpaceGameplayRadius());
+    }
 }
