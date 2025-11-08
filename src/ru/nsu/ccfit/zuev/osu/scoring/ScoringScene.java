486e4606378d40cbda845863776edd687e7306b1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-11-08 10:55:31 +0800

    Display Flashlight size multiplier in `ScoringScene`

--- src/ru/nsu/ccfit/zuev/osu/scoring/ScoringScene.java
+++ src/ru/nsu/ccfit/zuev/osu/scoring/ScoringScene.java
@@ -347,7 +347,7 @@ public class ScoringScene {
         playerStr += String.format("  %s(%s)", BuildConfig.VERSION_NAME, BuildConfig.BUILD_TYPE);
         if (mods.contains(ModCustomSpeed.class) ||
             mods.contains(ModDifficultyAdjust.class) ||
-            (mods.contains(ModFlashlight.class) && mods.ofType(ModFlashlight.class).getFollowDelay() != ModFlashlight.DEFAULT_FOLLOW_DELAY)) {
+            (mods.contains(ModFlashlight.class) && !mods.ofType(ModFlashlight.class).getUsesDefaultSettings())) {
 
             var customSpeed = mods.ofType(ModCustomSpeed.class);
             var difficultyAdjust = mods.ofType(ModDifficultyAdjust.class);
@@ -376,8 +376,14 @@ public class ScoringScene {
                 }
             }
 
-            if (flashlight != null && flashlight.getFollowDelay() != ModFlashlight.DEFAULT_FOLLOW_DELAY) {
-                mapperStr += String.format(Locale.ENGLISH, "FLD%.2f,", flashlight.getFollowDelay());
+            if (flashlight != null && !flashlight.getUsesDefaultSettings()) {
+                if (flashlight.getFollowDelay() != ModFlashlight.DEFAULT_FOLLOW_DELAY) {
+                    mapperStr += String.format(Locale.ENGLISH, "FLD%.2f,", flashlight.getFollowDelay());
+                }
+
+                if (flashlight.getSizeMultiplier() != ModFlashlight.DEFAULT_SIZE_MULTIPLIER) {
+                    mapperStr += String.format(Locale.ENGLISH, "%.2fx,", flashlight.getSizeMultiplier());
+                }
             }
 
             if (mapperStr.endsWith(",")){
