f55a416508feb71170b65d0629711242f5b6c66e
Reco1l <geronimoferruccio15@gmail.com>
2025-12-01 11:34:53 -0300

    Fix padding and size in RoomCreateDialog

--- src/com/osudroid/ui/v2/multi/RoomCreateDialog.kt
+++ src/com/osudroid/ui/v2/multi/RoomCreateDialog.kt
@@ -7,12 +7,11 @@ import com.reco1l.andengine.*
 import com.reco1l.andengine.component.*
 import com.reco1l.andengine.container.*
 import com.reco1l.andengine.theme.Size
-import com.reco1l.andengine.theme.rem
 import com.reco1l.andengine.theme.srem
 import com.reco1l.andengine.theme.vh
 import com.reco1l.andengine.ui.*
 import com.reco1l.andengine.ui.form.*
-import com.reco1l.framework.math.Vec2
+import com.reco1l.framework.math.Vec4
 import com.reco1l.toolkt.kotlin.*
 import ru.nsu.ccfit.zuev.osu.*
 import ru.nsu.ccfit.zuev.osu.helper.*
@@ -22,9 +21,11 @@ import ru.nsu.ccfit.zuev.osu.online.*
 class RoomCreateDialog(lobbyScene: LobbyScene) : UIDialog<UIScrollableContainer>(UIScrollableContainer().apply {
     scrollAxes = Axes.Y
     width = Size.Full
-    height = 0.5f.vh
     clipToBounds = true
-    scrollPadding = Vec2(0f, 4f.srem)
+    style = {
+        height = 0.55f.vh
+        padding = Vec4(2f.srem, 4f.srem)
+    }
 }) {
     init {
 
@@ -110,7 +111,7 @@ class RoomCreateDialog(lobbyScene: LobbyScene) : UIDialog<UIScrollableContainer>
 
         addButton(UITextButton().apply {
             setText(string.multiplayer_lobby_create_room_accept)
-            isSelected = true
+            colorVariant = ColorVariant.Primary
             onActionUp = {
                 form.submit()
             }
