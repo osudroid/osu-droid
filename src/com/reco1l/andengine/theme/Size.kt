dc19e31a2e407e6f9185b7a649883b0906cc8469
Reco1l <geronimoferruccio15@gmail.com>
2025-12-01 11:35:31 -0300

    Increase global corner radius by a bit

--- src/com/reco1l/andengine/theme/Size.kt
+++ src/com/reco1l/andengine/theme/Size.kt
@@ -3,19 +3,19 @@ package com.reco1l.andengine.theme
 
 object Radius {
     val XS
-        get() = 0.125f.rem
+        get() = 0.425f.rem
 
     val SM
-        get() = 0.25f.rem
+        get() = 0.45f.rem
 
     val MD
-        get() = 0.375f.rem
+        get() = 0.475f.rem
 
     val LG
         get() = 0.5f.rem
 
     val XL
-        get() = 0.75f.rem
+        get() = 0.525f.rem
 
     const val Full = Float.MAX_VALUE
 
