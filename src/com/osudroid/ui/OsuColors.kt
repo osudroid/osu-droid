b168d64124cd6794bde62285b14d534e0e17d6f6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-01-10 21:22:41 +0800

    Extract domain-range linear color interpolation in star rating color

--- src/com/osudroid/ui/OsuColors.kt
+++ src/com/osudroid/ui/OsuColors.kt
@@ -13,27 +13,27 @@ import kotlin.math.ceil
 object OsuColors {
 
     // Source from: https://github.com/ppy/osu/blob/b9b341affd4faea65cd29c9678385176628ace5d/osu.Game/Graphics/OsuColour.cs#L26
-
-    private val STAR_RATING_GRADIENT = listOf(
-        0.1f to Color4(0xFFaaaaaa.toInt()),
-        0.1f to Color4(0xFF4290fb.toInt()),
-        1.25f to Color4(0xFF4fc0ff.toInt()),
-        2.0f to Color4(0xFF4fffd5.toInt()),
-        2.5f to Color4(0xFF7cff4f.toInt()),
-        3.3f to Color4(0xFFf6f05c.toInt()),
-        4.2f to Color4(0xFFff8068.toInt()),
-        4.9f to Color4(0xFFff4e6f.toInt()),
-        5.8f to Color4(0xFFc645b8.toInt()),
-        6.7f to Color4(0xFF6563de.toInt()),
-        7.7f to Color4(0xFF18158e.toInt()),
-        9.0f to Color4(Color.BLACK)
+    private val starRatingColorSpectrum = LinearColorScale(
+        listOf(
+            0.1f to Color4(0xFFaaaaaa.toInt()),
+            0.1f to Color4(0xFF4290fb.toInt()),
+            1.25f to Color4(0xFF4fc0ff.toInt()),
+            2.0f to Color4(0xFF4fffd5.toInt()),
+            2.5f to Color4(0xFF7cff4f.toInt()),
+            3.3f to Color4(0xFFf6f05c.toInt()),
+            4.2f to Color4(0xFFff8068.toInt()),
+            4.9f to Color4(0xFFff4e6f.toInt()),
+            5.8f to Color4(0xFFc645b8.toInt()),
+            6.7f to Color4(0xFF6563de.toInt()),
+            7.7f to Color4(0xFF18158e.toInt()),
+            9.0f to Color4(Color.BLACK)
+        )
     )
 
     /**
      * Retrieves the colour for a given point in the star range.
      */
     fun getStarRatingColor(point: Double): Color4 {
-
         // Mimics the MidpointRounding.AwayFromZero rounding from C#
         val sr = ceil(point).toFloat().roundBy(2)
 
@@ -41,19 +41,7 @@ object OsuColors {
             return Color4(0xAAAAAA)
         }
 
-        for (i in 0 until STAR_RATING_GRADIENT.size - 1) {
-
-            val (startDomain, startColor) = STAR_RATING_GRADIENT[i]
-            val (endDomain, endColor) = STAR_RATING_GRADIENT[i + 1]
-
-            if (sr >= endDomain) {
-                continue
-            }
-
-            return Colors.interpolate(sr, startColor, endColor, startDomain, endDomain)
-        }
-
-        return Color.BLACK.toColor4()
+        return starRatingColorSpectrum.get(sr)
     }
 
     val green = Color4(0xFF88B300)
@@ -66,4 +54,34 @@ object OsuColors {
     val yellow = Color4(0xFFFFCC22)
     val yellowDark = Color4(0xFFEEAA00)
     val yellowDarker = Color4(0xFFCC6600)
+}
+
+private class LinearColorScale(
+    private val gradient: List<Pair<Float, Color4>>,
+    private val clamp: Boolean = true
+) {
+    fun get(point: Float): Color4 {
+        if (clamp) {
+            if (point <= gradient.first().first) {
+                return gradient.first().second
+            }
+
+            if (point >= gradient.last().first) {
+                return gradient.last().second
+            }
+        }
+
+        for (i in 0 until gradient.size - 1) {
+            val (startDomain, startColor) = gradient[i]
+            val (endDomain, endColor) = gradient[i + 1]
+
+            if (point >= endDomain) {
+                continue
+            }
+
+            return Colors.interpolate(point, startColor, endColor, startDomain, endDomain)
+        }
+
+        return gradient.last().second
+    }
 }
