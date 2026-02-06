41c4c458088c026fd746843cd13323f6ab8f60f3
Reco1l <geronimoferruccio15@gmail.com>
2026-02-06 15:26:08 -0300

    Add exit confirmation dialog and exit animation to MainScene

--- src/com/osudroid/ui/v2/mainmenu/OsuLogo.kt
+++ src/com/osudroid/ui/v2/mainmenu/OsuLogo.kt
@@ -6,6 +6,8 @@ import com.reco1l.andengine.Anchor
 import com.reco1l.andengine.circle
 import com.reco1l.andengine.component.ClearInfo
 import com.reco1l.andengine.component.DepthInfo
+import com.reco1l.andengine.component.rotationCenter
+import com.reco1l.andengine.component.scaleCenter
 import com.reco1l.andengine.container
 import com.reco1l.andengine.container.UIContainer
 import com.reco1l.andengine.shape.PaintStyle
@@ -24,15 +26,22 @@ import com.reco1l.framework.rgb
 
 class OsuLogo(withExternalEffects: Boolean = true) : UIClickableContainer() {
 
+    /**
+     * Whether to play music effects.
+     */
+    var playEffects = withExternalEffects
+
+
     private lateinit var osuText: UIText
+    private lateinit var inputFeedbackCircle: UICircle
 
     private val bounceContainer: UIContainer
 
-    private lateinit var inputFeedbackCircle: UICircle
+    private var radialVisualizer: RadialVisualizer? = null
+    private var rippleDispenser: RippleVisualizer? = null
 
 
     init {
-
         if (withExternalEffects) {
             +RippleVisualizer().apply {
                 width = Size.Full
@@ -40,6 +49,8 @@ class OsuLogo(withExternalEffects: Boolean = true) : UIClickableContainer() {
                 anchor = Anchor.Center
                 origin = Anchor.Center
                 alpha = 0.3f
+
+                rippleDispenser = this
             }
         }
 
@@ -56,6 +67,8 @@ class OsuLogo(withExternalEffects: Boolean = true) : UIClickableContainer() {
                     anchor = Anchor.Center
                     origin = Anchor.Center
                     alpha = 0.4f
+
+                    radialVisualizer = this
                 }
             }
 
@@ -71,7 +84,7 @@ class OsuLogo(withExternalEffects: Boolean = true) : UIClickableContainer() {
                 anchor = Anchor.Center
                 origin = Anchor.Center
                 color = OSU_COLOR
-                clearInfo = ClearInfo.Companion.ClearDepthBuffer
+                clearInfo = ClearInfo.ClearDepthBuffer
                 depthInfo = DepthInfo(test = true, mask = true, function = GLES10.GL_ALWAYS)
             }
 
@@ -131,14 +144,20 @@ class OsuLogo(withExternalEffects: Boolean = true) : UIClickableContainer() {
     override fun onManagedUpdate(deltaTimeSec: Float) {
         osuText.fontSize = height * 0.295f
 
-        val beatLengthSeconds = RythimManager.beatLength.toFloat() / 1000f
+        if (playEffects) {
+            val beatLengthSeconds = RythimManager.beatLength.toFloat() / 1000f
 
-        if (RythimManager.beatElapsed / 1000f < beatLengthSeconds * 0.75f) {
-            val threeQuarts = beatLengthSeconds * 0.75f
-            bounceContainer.setScale(Interpolation.floatAt(deltaTimeSec.coerceIn(0f, threeQuarts), bounceContainer.scaleX, 1f, 0f, threeQuarts))
+            if (RythimManager.beatElapsed / 1000f < beatLengthSeconds * 0.75f) {
+                val threeQuarts = beatLengthSeconds * 0.75f
+                bounceContainer.setScale(Interpolation.floatAt(deltaTimeSec.coerceIn(0f, threeQuarts), bounceContainer.scaleX, 1f, 0f, threeQuarts))
+            } else {
+                val oneQuart = beatLengthSeconds * 0.25f
+                bounceContainer.setScale(Interpolation.floatAt(deltaTimeSec.coerceIn(0f, oneQuart), bounceContainer.scaleX, 0.9f, 0f, oneQuart))
+            }
         } else {
-            val oneQuart = beatLengthSeconds * 0.25f
-            bounceContainer.setScale(Interpolation.floatAt(deltaTimeSec.coerceIn(0f, oneQuart), bounceContainer.scaleX, 0.9f, 0f, oneQuart))
+            bounceContainer.setScale(Interpolation.floatAt(deltaTimeSec.coerceIn(0f, 0.1f), bounceContainer.scaleX, 1f, 0f, 0.1f))
+            radialVisualizer?.alpha = Interpolation.floatAt(deltaTimeSec.coerceIn(0f, 0.1f), radialVisualizer?.alpha ?: 0f, 0f, 0f, 0.1f)
+            rippleDispenser?.alpha = Interpolation.floatAt(deltaTimeSec.coerceIn(0f, 0.1f), rippleDispenser?.alpha ?: 0f, 0f, 0f, 0.1f)
         }
 
         inputFeedbackCircle.alpha = Interpolation.floatAt(deltaTimeSec.coerceIn(0f, 0.1f), inputFeedbackCircle.alpha, 0f, 0f, 0.1f)
