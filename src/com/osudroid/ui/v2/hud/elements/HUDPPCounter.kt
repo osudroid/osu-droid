c9a6268d8d298a99519c6bed037d779f1cb4ed39
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-02-22 16:24:20 +0700

    Add rolling counter to PP counter

--- src/com/osudroid/ui/v2/hud/elements/HUDPPCounter.kt
+++ src/com/osudroid/ui/v2/hud/elements/HUDPPCounter.kt
@@ -2,6 +2,7 @@ package com.osudroid.ui.v2.hud.elements
 
 import com.osudroid.ui.v2.hud.HUDElement
 import com.osudroid.ui.v2.SpriteFont
+import com.rian.framework.RollingDoubleCounter
 import ru.nsu.ccfit.zuev.osu.Config
 import ru.nsu.ccfit.zuev.osu.DifficultyAlgorithm
 import ru.nsu.ccfit.zuev.osu.game.GameScene
@@ -14,25 +15,35 @@ class HUDPPCounter : HUDElement() {
 
     private val sprite = SpriteFont(OsuSkin.get().scorePrefix)
 
-    private var value = -1.0
+    private var value = 0
         set(value) {
             if (field != value) {
                 field = value
-
-                // The NaN check here is necessary as calculations may result in NaN values, which should not be displayed.
-                // This is not a foolproof solution, but is enough to prevent crashes from happening.
-                // Until the source of the NaN values is found and fixed, this must remain in place.
-                sprite.text = "${(value.takeIf { !it.isNaN() } ?: 0.0).roundToInt()}${if (Config.getDifficultyAlgorithm() == DifficultyAlgorithm.droid) "dpp" else "pp"}"
+                updateText()
             }
         }
 
+    private val counter = RollingDoubleCounter(0.0).apply {
+        rollingDuration = 1000f
+    }
+
     init {
-        value = 0.0
         attachChild(sprite)
+        updateText()
         onContentChanged()
     }
 
     override fun onGameplayUpdate(game: GameScene, secondsElapsed: Float) {
-        value = game.stat.pp
+        counter.update(secondsElapsed * 1000)
+
+        // The NaN check here is necessary as calculations may result in NaN values, which should not be displayed.
+        // This is not a foolproof solution, but is enough to prevent crashes from happening.
+        // Until the source of the NaN values is found and fixed, this must remain in place.
+        counter.targetValue = game.stat.pp.takeIf { !it.isNaN() } ?: 0.0
+        value = counter.currentValue.roundToInt()
+    }
+
+    private fun updateText() {
+        sprite.text = "${value}${if (Config.getDifficultyAlgorithm() == DifficultyAlgorithm.droid) "dpp" else "pp"}"
     }
 }
