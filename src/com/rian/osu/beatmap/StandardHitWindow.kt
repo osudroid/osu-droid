c2851f97149e52c4d492609f6d954097c6a98823
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-04 12:10:05 +0700

    Apply flooring and half-millisecond adjustments to osu!standard hit windows

--- src/com/rian/osu/beatmap/StandardHitWindow.kt
+++ src/com/rian/osu/beatmap/StandardHitWindow.kt
@@ -1,5 +1,7 @@
 package com.rian.osu.beatmap
 
+import kotlin.math.floor
+
 /**
  * Represents the osu!standard hit window.
  */
@@ -10,40 +12,11 @@ class StandardHitWindow @JvmOverloads constructor(
     overallDifficulty: Float? = 5f
 ) : HitWindow(overallDifficulty) {
     override val greatWindow
-        get() = 80 - 6 * overallDifficulty
+        get() = floor(80 - 6 * overallDifficulty) - 0.5f
 
     override val okWindow
-        get() = 140 - 8 * overallDifficulty
+        get() = floor(140 - 8 * overallDifficulty) - 0.5f
 
     override val mehWindow
-        get() = 200 - 10 * overallDifficulty
-
-    companion object {
-        /**
-         * Calculates the overall difficulty value of a great hit window.
-         *
-         * @param value The value of the hit window in milliseconds.
-         * @return The overall difficulty value.
-         */
-        @JvmStatic
-        fun hitWindow300ToOverallDifficulty(value: Float) = (80 - value) / 6
-
-        /**
-         * Calculates the overall difficulty value of an ok hit window.
-         *
-         * @param value The value of the hit window in milliseconds.
-         * @returns The overall difficulty value.
-         */
-        @JvmStatic
-        fun hitWindow100ToOverallDifficulty(value: Float) = (140 - value) / 8
-
-        /**
-         * Calculates the overall difficulty value of a meh hit window.
-         *
-         * @param value The value of the hit window in milliseconds.
-         * @returns The overall difficulty value.
-         */
-        @JvmStatic
-        fun hitWindow50ToOverallDifficulty(value: Float) = (200 - value) / 10
-    }
+        get() = floor(200 - 10 * overallDifficulty) - 0.5f
 }
