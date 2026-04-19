cb81857d8d3c3aebf06a1ab4b58f25d890102762
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-19 20:39:16 +0800

    Add aim agility evaluators

---
+++ src/com/rian/osu/difficulty/evaluators/DroidAgilityEvaluator.kt
@@ -0,0 +1,42 @@
+package com.rian.osu.difficulty.evaluators
+
+import com.rian.osu.beatmap.hitobject.Spinner
+import com.rian.osu.difficulty.DifficultyHitObject
+import com.rian.osu.difficulty.DroidDifficultyHitObject
+import kotlin.math.min
+import kotlin.math.pow
+
+/**
+ * An evaluator for calculating osu!droid agility aim difficulty.
+ */
+object DroidAgilityEvaluator {
+    private val distanceCap = DifficultyHitObject.NORMALIZED_DIAMETER * 1.2
+
+    /**
+     * Evaluates the difficulty of fast aiming the current object.
+     *
+     * @param current The current object.
+     */
+    @JvmStatic
+    fun evaluateDifficultyOf(current: DroidDifficultyHitObject): Double {
+        if (current.obj is Spinner) {
+            return 0.0
+        }
+
+        val prev = current.previous(0)
+
+        val travelDistance = prev?.lazyTravelDistance ?: 0.0
+        val distance = travelDistance + current.lazyJumpDistance
+
+        val distanceScaled = min(distance, distanceCap) / distanceCap
+
+        var strain = distanceScaled * 1000 / current.strainTime
+
+        strain *= current.smallCircleBonus.pow(1.5)
+        strain *= highBpmBonus(current.strainTime)
+
+        return strain
+    }
+
+    private fun highBpmBonus(ms: Double) = 1 / (1 - 0.2.pow(ms / 1000))
+}
