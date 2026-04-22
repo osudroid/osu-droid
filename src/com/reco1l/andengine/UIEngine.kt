07f993534135830edec023af099a99dd33ca9b11
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-22 23:58:40 +0800

    Set `UIEngine` overlay touch traversal to front-to-back
    Matches `UIScene`.

--- src/com/reco1l/andengine/UIEngine.kt
+++ src/com/reco1l/andengine/UIEngine.kt
@@ -21,7 +21,9 @@ class UIEngine(val context: Activity, options: EngineOptions) : Engine(options) 
     /**
      * The global HUD used for overlays (menus, dialogs, etc).
      */
-    val overlay = HUD()
+    val overlay = HUD().apply {
+        setOnAreaTouchTraversalFrontToBack()
+    }
 
     /**
      * The resource manager for loading and accessing UI resources (fonts, textures, etc).
