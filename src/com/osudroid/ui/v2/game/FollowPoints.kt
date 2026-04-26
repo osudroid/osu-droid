863278cdbd5c69086ba4686570e00ffbfcd2f38c
acivev <marko-9970@live.de>
2026-04-26 16:23:37 +0200

    Refactor texture size adjustment in applyFollowPointMaxSize for improved clarity and accuracy

--- src/com/osudroid/ui/v2/game/FollowPoints.kt
+++ src/com/osudroid/ui/v2/game/FollowPoints.kt
@@ -56,16 +56,16 @@ object FollowPointConnection {
 
     private fun TextureRegion.applyFollowPointMaxSize() {
         // Reference: https://github.com/ppy/osu/blob/0811de728e4205a45e485d53ccdaf19a937c6033/osu.Game.Rulesets.Osu/Skinning/Legacy/OsuLegacySkinTransformer.cs#L95-L97
-        val newWidth = min(width, HitObject.OBJECT_RADIUS.toInt() * 2)
-        val newHeight = min(height, HitObject.OBJECT_RADIUS.toInt())
+        val newWidth = min(width, HitObject.OBJECT_RADIUS * 2)
+        val newHeight = min(height, HitObject.OBJECT_RADIUS)
 
         if (width != newWidth || height != newHeight) {
 
             // Crop the texture from the center.
             setTexturePosition(width / 2 - newWidth / 2, height / 2 - newHeight / 2)
 
-            width = newWidth
-            height = newHeight
+            setTextureWidth(newWidth)
+            setTextureHeight(newHeight)
         }
     }
 
