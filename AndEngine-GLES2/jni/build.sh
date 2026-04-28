dab334b3a5caac1cb7d246434a525b71ca3fca11
acivev <marko-9970@live.de>
2026-04-28 16:58:30 +0200

    chore(jni): make build.sh portable using ANDROID_NDK_HOME and relative paths

--- AndEngine-GLES2/jni/build.sh
+++ AndEngine-GLES2/jni/build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+set -e
 
-NDK_DIRECTORY="/Users/ngramlich/SDKs/Android/ndk/r7b/"
-PROJECT_DIRECTORY="/Users/ngramlich/Workspace/gdk/graphic_engines/AndEngine/AndEngine/"
+SCRIPT_DIRECTORY="$(cd "$(dirname "$0")" && pwd)"
+NDK_DIRECTORY="${ANDROID_NDK_HOME:-${ANDROID_NDK_ROOT:-}}"
+PROJECT_DIRECTORY="$(cd "${SCRIPT_DIRECTORY}/.." && pwd)"
+
+if [ -z "${NDK_DIRECTORY}" ]; then
+	echo "ANDROID_NDK_HOME (or ANDROID_NDK_ROOT) is not set."
+	exit 1
+fi
 
 # Run build:
-pushd ${PROJECT_DIRECTORY}
-${NDK_DIRECTORY}ndk-build
+pushd "${PROJECT_DIRECTORY}"
+"${NDK_DIRECTORY}/ndk-build"
 
 # Clean temporary files:
-# rm -rf ${PROJECT_DIRECTORY}obj
+# rm -rf "${PROJECT_DIRECTORY}/obj"
 # find . -name gdbserver -print0 | xargs -0 rm -rf
 # find . -name gdb.setup -print0 | xargs -0 rm -rf
 
