86268735f1ead30d826fae6349c1badd646f318c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-07 20:41:43 +0800

    Add documentation to `Stopwatch`

--- src/com/rian/andengine/timing/Stopwatch.kt
+++ src/com/rian/andengine/timing/Stopwatch.kt
@@ -13,6 +13,9 @@ open class Stopwatch {
     private var elapsedNanos = 0L
     private var startTime = 0L
 
+    /**
+     * Starts this [Stopwatch]. If this [Stopwatch] is already running, this method does nothing.
+     */
     open fun start() {
         if (!isRunning) {
             startTime = System.nanoTime()
@@ -20,6 +23,9 @@ open class Stopwatch {
         }
     }
 
+    /**
+     * Stops this [Stopwatch]. If this [Stopwatch] is already stopped, this method does nothing.
+     */
     open fun stop() {
         if (isRunning) {
             elapsedNanos += System.nanoTime() - startTime
@@ -27,16 +33,26 @@ open class Stopwatch {
         }
     }
 
+    /**
+     * Resets this [Stopwatch] to its initial state. If this [Stopwatch] is running, it will be stopped.
+     */
     open fun reset() {
         elapsedNanos = 0
         isRunning = false
     }
 
+    /**
+     * Restarts this [Stopwatch] by resetting it and then starting it.
+     */
     open fun restart() {
         reset()
         start()
     }
 
+    /**
+     * The total elapsed time in seconds. If this [Stopwatch] is currently running, the elapsed time will include the
+     * time since it was last started.
+     */
     val elapsedSeconds: Float
         get() {
             val currentElapsed = if (isRunning) elapsedNanos + (System.nanoTime() - startTime) else elapsedNanos
