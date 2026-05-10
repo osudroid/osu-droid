441f62a39a9831c131959ad1885a587ba857ce0e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-10 18:46:33 +0800

    Fix `OffsetClock` delegation

--- src/com/rian/andengine/timing/OffsetClock.kt
+++ src/com/rian/andengine/timing/OffsetClock.kt
@@ -6,6 +6,9 @@ open class OffsetClock(protected var source: IClock) : IClock {
     override val currentTime
         get() = source.currentTime + offset
 
-    override val rate by source::rate
-    override val isRunning by source::isRunning
+    override val rate
+        get() = source.rate
+
+    override val isRunning
+        get() = source.isRunning
 }
