2584740fa82f95ab4d5ca8335ac6ea9ab38492d5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-12 11:26:36 +0800

    Add load state to `HUD`

--- src/com/rian/andengine/HUD.kt
+++ src/com/rian/andengine/HUD.kt
@@ -1,5 +1,6 @@
 package com.rian.andengine
 
+import com.reco1l.andengine.LoadState
 import com.reco1l.andengine.UIEngine
 import com.reco1l.andengine.component.UIComponent
 import com.reco1l.toolkt.kotlin.fastForEach
@@ -51,6 +52,17 @@ open class HUD : AndEngineHUD(), IClockProvider<IFrameBasedClock?>, IClockReceiv
         inheritedClock = clock
         val currentClock = this.clock
 
+        if (currentClock != null) {
+            if (loadState == LoadState.NotLoaded) {
+                loadState = LoadState.Ready
+            }
+        } else {
+            if (loadState == LoadState.Loaded) {
+                onUnload()
+            }
+            loadState = LoadState.NotLoaded
+        }
+
         mChildren?.fastForEach {
             @Suppress("UNCHECKED_CAST")
             (it as? IClockReceiver<IFrameBasedClock?>)?.updateClock(currentClock)
@@ -60,18 +72,58 @@ open class HUD : AndEngineHUD(), IClockProvider<IFrameBasedClock?>, IClockReceiv
         (mChildScene as? IClockReceiver<IFrameBasedClock?>)?.updateClock(currentClock)
     }
 
+    //region Update
+
     final override fun onUpdate(deltaTimeSec: Float) {
+        if (loadState == LoadState.NotLoaded) {
+            return
+        }
+
         if (processCustomClock) {
             customClock?.processFrame()
         }
 
-        if (isIgnoreUpdate) {
-            return
+        if (loadState == LoadState.Ready) {
+            loadState = LoadState.Loaded
+            onLoadComplete()
         }
 
-        // Fallback to parent or engine-provided delta time in case clock is not present.
-        onManagedUpdate(clock?.elapsedFrameTime ?: deltaTimeSec)
+        if (!isIgnoreUpdate) {
+            // Fallback to parent or engine-provided delta time in case clock is not present.
+            onManagedUpdate(clock?.elapsedFrameTime ?: deltaTimeSec)
+        }
     }
+
+    /**
+     * Whether this [HUD] is currently loaded and part of the active update loop.
+     *
+     * This becomes `true` after [onLoadComplete] is called and stays `true` until this [HUD] is unloaded.
+     */
+    val isLoaded
+        get() = loadState == LoadState.Loaded
+
+    private var loadState = LoadState.NotLoaded
+
+    /**
+     * Called when this [HUD] is fully loaded and ready for use.
+     *
+     * This is invoked when [onUpdate] is called for the first time after this [HUD] receives a valid [clock] **and**
+     * before [onManagedUpdate]. It is safe to start animations and modifiers here.
+     *
+     * Note that this is called regardless of [isIgnoreUpdate], and can be called multiple times during this [HUD]'s
+     * lifetime if it is detached and re-attached.
+     */
+    protected open fun onLoadComplete() {}
+
+    /**
+     * Called when this [HUD] is being unloaded.
+     *
+     * This is invoked when this [HUD] loses its [clock], usually when it is detached from its [parent]. If this [HUD]
+     * is re-attached, [onLoadComplete] will be called again.
+     */
+    protected open fun onUnload() {}
+
+    //endregion
 
     override fun setChildScene(childScene: Scene?, modalDraw: Boolean, modalUpdate: Boolean, modalTouch: Boolean) {
         this.childScene?.onDetached()
