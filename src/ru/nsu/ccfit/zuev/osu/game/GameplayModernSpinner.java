777535fc08701b4cf540c31c05880b1511621fab
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-13 13:08:51 +0800

    Fix modern spinners potentially not receiving complete rotations in replays

--- src/ru/nsu/ccfit/zuev/osu/game/GameplayModernSpinner.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameplayModernSpinner.java
@@ -312,15 +312,11 @@ public class GameplayModernSpinner extends GameplaySpinner {
 
         int score = 0;
         if (replayObjectData != null) {
-            if (fullRotations < replayObjectData.accuracy / 4)
-                fullRotations = replayObjectData.accuracy / 4;
-            if (fullRotations >= needRotations)
-                clear = true;
-            int bonusRot = (int) (replayObjectData.accuracy / 4f - needRotations + 1);
-            while (bonusRot < score) {
-                bonusRot++;
+            while (fullRotations + bonusScoreCounter < replayObjectData.accuracy / 4 + 1) {
+                fullRotations++;
                 listener.onSpinnerHit(id, 1000, false, 0);
             }
+            clear = fullRotations >= needRotations;
         }
         float percentFilled = (Math.abs(rotations) + fullRotations)
                 / needRotations;
