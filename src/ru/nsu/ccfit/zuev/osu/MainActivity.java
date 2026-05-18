d919f65400ea84331a0c3b5bf61a0bf8dc9cd1d6
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-18 19:59:21 +0800

    Fix nonexistent string reference

--- src/ru/nsu/ccfit/zuev/osu/MainActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/MainActivity.java
@@ -434,7 +434,7 @@ public class MainActivity extends BaseGameActivity implements
                 // LibraryManager.INSTANCE.sort();
                 fileToAdd = null;
             } else if (file.getName().toLowerCase().endsWith(".osk")) {
-                ToastLogger.showText(StringTable.get(com.osudroid.resources.R.string.library_skin_importing), false);
+                ToastLogger.showText(StringTable.get(R.string.library_skin_importing), false);
                 FileUtils.extractZip(fileToAdd, Config.getSkinTopPath());
                 fileToAdd = null;
             } else if (file.getName().endsWith(".odr")) {
