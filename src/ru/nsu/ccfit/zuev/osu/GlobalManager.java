5305452bb69e3dc2568cbb2f01b667701d185cdc
acivev <marko-9970@live.de>
2026-05-20 16:26:44 +0200

    refactor: replace MainScene with MainMenuV2 in GlobalManager and drop update-thread latch

--- src/ru/nsu/ccfit/zuev/osu/GlobalManager.java
+++ src/ru/nsu/ccfit/zuev/osu/GlobalManager.java
@@ -3,12 +3,9 @@ package ru.nsu.ccfit.zuev.osu;
 import com.acivev.ui.menu.main.MainMenuV2;
 import com.osudroid.data.BeatmapInfo;
 import com.osudroid.data.DatabaseManager;
-import com.osudroid.utils.Execution;
 import com.reco1l.andengine.UIEngine;
 
 import org.andengine.engine.camera.Camera;
-
-import java.util.concurrent.CountDownLatch;
 
 import ru.nsu.ccfit.zuev.audio.serviceAudio.SaveServiceObject;
 import ru.nsu.ccfit.zuev.audio.serviceAudio.SongService;
@@ -24,7 +21,6 @@ public class GlobalManager {
     private UIEngine engine;
     private Camera camera;
     private GameScene gameScene;
-    private MainScene mainScene;
     private MainMenuV2 mainMenuV2;
     private ScoringScene scoring;
     private SongMenu songMenu;
@@ -56,26 +52,10 @@ public class GlobalManager {
         songService = saveServiceObject.getSongService();
         SongService.initBASS();
         setLoadingProgress(10);
-        setMainScene(new MainScene());
+        setMainMenuV2(new MainMenuV2());
         setInfo("Loading skin...");
         ResourceManager.getInstance().loadSkin(Config.getSkinPath());
-        // Construct MainMenuV2 after loadSkin() so its eager getTexture()/getFont()
-        // calls resolve skin-provided resources rather than fallbacks.
-        // Construction is posted to the update thread because MainMenuV2's init block
-        // performs scene-graph mutations (attachChild) that must not run off-thread.
-        // The latch ensures init() does not return until mainMenuV2 is ready,
-        // so callers cannot observe a null mainMenuV2 after init() completes.
-        CountDownLatch latch = new CountDownLatch(1);
-        Execution.updateThread(() -> {
-            setMainMenuV2(new MainMenuV2());
-            setLoadingProgress(30);
-            latch.countDown();
-        });
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        setLoadingProgress(30);
         setGameScene(new GameScene(getEngine()));
         setSongMenu(new SongMenu());
         setLoadingProgress(40);
@@ -112,14 +92,6 @@ public class GlobalManager {
 
     public void setGameScene(GameScene gameScene) {
         this.gameScene = gameScene;
-    }
-
-    public MainScene getMainScene() {
-        return mainScene;
-    }
-
-    public void setMainScene(MainScene mainScene) {
-        this.mainScene = mainScene;
     }
 
     public MainMenuV2 getMainMenuV2() {
