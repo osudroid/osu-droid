14061c4346f5b4efafc2577a8e216a5fd658c4b5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-09-07 18:12:00 +0700

    Fix KDoc of `deserializeMods` in `ModHashMap`

--- src/com/osudroid/utils/ModHashMap.kt
+++ src/com/osudroid/utils/ModHashMap.kt
@@ -157,7 +157,7 @@ open class ModHashMap : ConcurrentHashMap<Class<out Mod>, Mod> {
     /**
      * Serializes all [Mod]s in this [ModHashMap] into a list of [APIMod]s, contained within a JSON string.
      *
-     * The result can be deserialized using [ModUtils.deserializeMods].
+     * The result can be deserialized using [deserializeMods].
      *
      * @param includeNonUserPlayable Whether to include [Mod]s whose [Mod.isUserPlayable] is `false`. Defaults to `true`.
      * @param includeIrrelevantMods Whether to include [Mod]s whose [Mod.isRelevant] is `false`. Defaults to `false`.
